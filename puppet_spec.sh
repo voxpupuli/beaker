ea22123545918fe8c838c2ef8e63a0af64451895
Jesse Wolfe <jes5199@gmail.com>
2010-06-28 14:33:41 -0700

    Pretty output

--- puppet_spec.sh
+++ puppet_spec.sh
@@ -3,7 +3,13 @@ let FAILURES=0
 let TOTAL=0
 
 for SPEC in `find ./spec -name '*_spec.sh'` ; do
-	bash $SPEC || let "FAILURES+=1"
+        if bash $SPEC ; then
+                echo -n . 
+        else
+                let "FAILURES+=1"
+                echo -n F
+        fi
         let "TOTAL+=1"
 done  
+echo
 echo "$TOTAL tests, $FAILURES failures"
