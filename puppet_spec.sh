d59482ec8973127e8c0580bb41c778816f5b32ac
Jesse Wolfe <jes5199@gmail.com>
2010-06-28 14:23:35 -0700

    We can run a spec

--- puppet_spec.sh
+++ puppet_spec.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
-for SPEC in spec/**/*_spec.sh ; do
-	bash $SPEC 
-done 
+let FAILURES=0
+let TOTAL=0
+
+for SPEC in `find ./spec -name '*_spec.sh'` ; do
+	bash $SPEC || let "FAILURES+=1"
+        let "TOTAL+=1"
+done  
+echo "$TOTAL tests, $FAILURES failures"
