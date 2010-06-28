c42d5fc1bede4a8b182a415848a6fc11501df701
Jesse Wolfe <jes5199@gmail.com>
2010-06-28 15:02:49 -0700

    failure log of what files failed

--- puppet_spec.sh
+++ puppet_spec.sh
@@ -8,10 +8,14 @@ fi
 let FAILURES=0
 let TOTAL=0
 
+FAIL_LOG=/tmp/$$.failures.txt
+rm -f $FAIL_LOG
+
 for SPEC in `find ./spec -name '*_spec.sh'` ; do
         if bash $SPEC >& /dev/null ; then
                 echo -n . 
         else
+                echo $SPEC > $FAIL_LOG
                 let "FAILURES+=1"
                 echo -n F
         fi
@@ -19,5 +23,6 @@ for SPEC in `find ./spec -name '*_spec.sh'` ; do
 done
 echo
 echo "$TOTAL tests, $FAILURES failures"
+cat -n $FAIL_LOG
 
 [ $FAILURES -eq 0 ]
