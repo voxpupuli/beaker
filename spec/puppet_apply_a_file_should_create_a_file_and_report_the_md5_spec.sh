ac9690c6033e6da2be867b1f4665640dbb486700
Jesse Wolfe <jes5199@gmail.com>
2010-06-28 15:23:57 -0700

    test actually using a manifest to create a file

---
+++ spec/puppet_apply_a_file_should_create_a_file_and_report_the_md5_spec.sh
@@ -0,0 +1,13 @@
+set -e
+
+. local_setup.sh
+
+rm -f /tmp/hello.world.$$.txt
+
+$BIN/puppet apply <<PP | grep "defined content as '{md5}098f6bcd4621d373cade4e832627b4f6'"
+file{ "/tmp/hello.world.$$.txt":
+        content => "test",
+}
+PP
+
+[ -f /tmp/hello.world.$$.txt ]
