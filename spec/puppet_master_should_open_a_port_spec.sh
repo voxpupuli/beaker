cb3e8e0e1ec507b069909512a34272c0149eb87c
Jesse Wolfe <jes5199@gmail.com>
2010-06-28 15:54:50 -0700

    puppet master should open a port

---
+++ spec/puppet_master_should_open_a_port_spec.sh
@@ -0,0 +1,20 @@
+. local_setup.sh
+
+PORT=18140
+
+$BIN/puppet master --masterport $PORT --vardir /tmp/puppet-$$ --confdir /tmp/puppet-$$ --rundir /tmp/puppet-$$ --no-daemonize &
+PUPPET_PID=$!
+
+for I in `seq 0 10` ; do
+        if lsof -i -n -P | grep '\*:'$PORT | grep $PUPPET_PID ; then
+                let status=0
+                break
+        else
+                let status=1
+                sleep 1
+        fi
+done
+
+kill $PUPPET_PID
+
+exit $status
