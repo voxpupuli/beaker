e18b8be59f4cea6d2c7bff84516ae025d8706054
Dan Bode <dan@reductivelabs.com>
2010-07-02 02:03:32 -0400

    puppet resource tests for host

---
+++ spec/resource/host/should_create_aliases.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+set -u
+
+. ../../../local_setup.sh
+
+if [ -f /tmp/hosts-$$ ]; then
+  rm /tmp/hosts-$$
+fi
+
+$BIN/puppet resource host test1 ensure=present ip=127.0.0.2 target="/tmp/host-$$" host_aliases=alias1
+egrep '^127.0.0.2[[:space:]]+test1[[:space:]]+alias1' /tmp/host-$$
---
+++ spec/resource/host/should_destroy.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+set -u
+
+. ../../../local_setup.sh
+
+PATTERN='^127.0.0.2[[:space:]]+test1'
+FILE="/tmp/hosts-$$"
+
+echo '127.0.0.2 test1' > $FILE
+
+$BIN/puppet resource host test1 ensure=absent ip=127.0.0.2 target="$FILE"
+! egrep $PATTERN $FILE
