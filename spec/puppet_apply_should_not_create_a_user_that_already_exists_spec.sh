021bfc0a3fe2aa3e5ff4c11c13978248d43e6b0e
Jesse Wolfe <jes5199@gmail.com>
2010-07-06 17:49:03 -0700

    make the ticket #4149 more verbose

--- spec/puppet_apply_should_not_create_a_user_that_already_exists_spec.sh
+++ spec/puppet_apply_should_not_create_a_user_that_already_exists_spec.sh
@@ -2,8 +2,10 @@ set -e
 
 . local_setup.sh
 
-! $BIN/puppet apply --debug <<PP | grep 'created'
+$BIN/puppet apply --debug <<PP | tee /tmp/puppet-$$.log
 user{ "root":
         ensure => "present",
 }
 PP
+
+! grep created /tmp/puppet-$$.log
