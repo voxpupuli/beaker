c4617794cafe50130f4e7918df1235056c896b58
Jesse Wolfe <jes5199@gmail.com>
2010-07-07 16:01:46 -0700

    Add a concept of a skipped test

--- puppet_spec.sh
+++ puppet_spec.sh
@@ -16,6 +16,7 @@ fi
 let FAILURES=0
 let TOTAL=0
 let PENDING=0
+let SKIPPED=0
 
 FAIL_LOG=/tmp/$$.failures.txt
 touch $FAIL_LOG
@@ -29,9 +30,15 @@ for SPEC in `find $TEST_DIR -name '*_spec.sh' ` ; do
         if $SPEC >& /dev/null ; then
                 echo -n .
         else
-                echo $SPEC >> $FAIL_LOG
-                let "FAILURES+=1"
-                echo -n F
+                let TEST_ERROR=$?
+                if [ $TEST_ERROR -eq 11 ] ; then
+                        echo -n -
+                        let "SKIPPED+=1"
+                else
+                        echo $SPEC >> $FAIL_LOG
+                        let "FAILURES+=1"
+                        echo -n F
+                fi
         fi
         let "TOTAL+=1"
 done
@@ -39,6 +46,9 @@ echo
 echo -n "$TOTAL tests, $FAILURES failures"
 if [ "$PENDING" -ne 0 ] ; then
         echo -n ", $PENDING pending"
+fi
+if [ "$SKIPPED" -ne 0 ] ; then
+        echo -n ", $SKIPPED skipped"
 fi
 echo
 
