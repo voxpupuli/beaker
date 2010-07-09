100fe4279f3f42f87862063024eb934b58a23355
Jesse Wolfe <jes5199@gmail.com>
2010-07-09 13:19:30 -0700

    fix broken symlink spec

--- spec/resource/file/should_create_symlink_spec.sh
+++ spec/resource/file/should_create_symlink_spec.sh
@@ -6,19 +6,20 @@ set -e
 set -u
 . local_setup.sh
 
-FILENAME="/tmp/$0-$$-link"
-FILENAME2="/tmp/$0-$$-file"
+FILENAME="/tmp/spec-$$-link"
+FILENAME2="/tmp/spec-$$-file"
 
 # preconditions:
 # 1. file should not exist
 if [ -e $FILENAME ]; then
-  rm -f FILENAME
+  rm -f $FILENAME
 fi
 if [ ! -e $FILENAME2 ]; then
-  touch $FILENAME2
+  echo hello$$ > $FILENAME2
 fi
 
 # run ralsh to create file
 $BIN/puppet resource file $FILENAME ensure=$FILENAME2
-# file should have size zero
-[ -L $FILENAME ] && 
+
+# file should have copied the contents
+grep hello$$ $FILENAME
