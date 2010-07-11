8a0d48bddab17ceef5fa42b4e2c5f847b1e1b4c1
Dan Bode <dan@reductivelabs.com>
2010-07-11 04:09:12 -0400

    fixed test failure, added PID to groupname to decrease likelihood of test failure.

--- spec/resource/group/should_create_with_gid_spec.sh
+++ spec/resource/group/should_create_with_gid_spec.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-
-set -e
-set -u
+#
+# test that we can set a groups gid
+#
 
 . local_setup.sh
 
-if getent group bozo; then
-  groupdel bozo
+#
+# precondition
+#  - group should not exist
+GROUP=bozo$$
+if getent group $GROUP; then
+  groupdel $GROUP
 fi
 
-$BIN/puppet resource group bozo ensure=present gid=12768
-getent group bozo | grep 12768
+$BIN/puppet resource group $GROUP ensure=present gid=12768
+getent group $GROUP | grep 12768
