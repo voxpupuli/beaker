ea28f450eabd78d41b6e7dc5a90c387200bfe49c
Jesse Wolfe <jes5199@gmail.com>
2010-07-20 16:18:21 -0700

    pending out tests that are using the wrong driver

--- spec/drivers.sh
+++ spec/drivers.sh
@@ -2,55 +2,77 @@
 . spec/util.sh
 
 function driver_standalone {
-        function execute_manifest {
-                cat | $BIN/puppet apply --confdir /tmp/puppet-$$-standalone --debug "$@" 
-        }
+    function execute_manifest {
+        mkdir -p /tmp/puppet-$$-standalone/manifests
+        cat | $BIN/puppet apply --confdir /tmp/puppet-$$-standalone --debug --manifestdir /tmp/puppet-$$-standalone/manifests "$@" 
+    }
 
-        function puppet_conf {
-                cat > /tmp/puppet-$$/puppet.conf
-        }
+    function puppet_conf {
+        cat > /tmp/puppet-$$/puppet.conf
+    }
+    
+    function manifest_file {
+        mkdir -p /tmp/puppet-$$-standalone/manifests
+        cat > /tmp/puppet-$$-standalone/manifests/$1
+    }
 }
 
 function driver_standalone_using_files {
-        driver_standalone # sort of like inherits
-        function execute_manifest {
-                cat > /tmp/manifest-$$.pp
-                $BIN/puppet apply --confdir /tmp/puppet-$$-standalone --debug "$@" /tmp/manifest-$$.pp
-        }
+    driver_standalone # sort of like inherits
+    
+    function execute_manifest {
+        cat > /tmp/manifest-$$.pp
+        mkdir -p /tmp/puppet-$$-standalone/manifests
+        $BIN/puppet apply --confdir /tmp/puppet-$$-standalone --debug  --manifestdir /tmp/puppet-$$-standalone/manifests "$@" /tmp/manifest-$$.pp
+    }
 
 }
 
 function driver_master_and_agent_locally {
-        mkdir -p /tmp/puppet-$$-master/manifests/
-        mkdir -p /tmp/puppet-$$-agent
+    mkdir -p /tmp/puppet-$$-master/manifests/
+    mkdir -p /tmp/puppet-$$-agent
+    
+    function execute_manifest {
+        start_puppet_master
+        cat > /tmp/puppet-$$-master/manifests/site.pp
+        start_puppet_agent "$@"
+        stop_puppet_master
+    }
 
-        function execute_manifest {
-                start_puppet_master
-                cat > /tmp/puppet-$$-master/manifests/site.pp
-                start_puppet_agent "$@"
-                stop_puppet_master
-        }
+    function manifest_file {
+        mkdir -p /tmp/puppet-$$-master/manifests
+        cat > /tmp/puppet-$$-master/manifests/$1
+    }
 
-        function puppet_conf {
-                cat | tee /tmp/puppet-$$-master/puppet.conf > /tmp/puppet-$$-agent/puppet.conf
-        }
+    function puppet_conf {
+        cat | tee /tmp/puppet-$$-master/puppet.conf > /tmp/puppet-$$-agent/puppet.conf
+    }
 }
 
 function driver_master_and_agent_locally_using_old_executables {
-        driver_master_and_agent_locally # sort of like inherits
-
-        function execute_manifest {
-                start_puppetmasterd
-                cat > /tmp/puppet-$$-master/manifests/site.pp
-                start_puppetd "$@"
-                stop_puppetmasterd
-        }
+    driver_master_and_agent_locally # sort of like inherits
+    
+    function execute_manifest {
+        start_puppetmasterd
+        cat > /tmp/puppet-$$-master/manifests/site.pp
+        start_puppetd "$@"
+        stop_puppetmasterd
+    }
 }
 
 
 function env_driver {
-        if [ -z "${PUPPET_SPEC_DRIVER:-}" ] ; then
-                PUPPET_SPEC_DRIVER=standalone
-        fi
-        driver_$PUPPET_SPEC_DRIVER
+    if [ -z "${PUPPET_ACCEPTANCE_DRIVER:-}" ] ; then
+        PUPPET_ACCEPTANCE_DRIVER=standalone
+    fi
+    driver_$PUPPET_ACCEPTANCE_DRIVER
 }
+
+function use_driver { 
+    if [ -z "$PUPPET_ACCEPTANCE_DRIVER" ] || [ "$PUPPET_ACCEPTANCE_DRIVER" = "$1" ] ; then
+        driver_$1
+    else
+        NOT_APPLICABLE
+    fi
+}
+
