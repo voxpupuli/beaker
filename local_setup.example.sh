7e2fea573dde4f7e82c2e936a4c020e2d4055e9d
Jeff McCune <jeff@puppetlabs.com>
2010-07-21 10:50:25 -0700

    Updated local_setup.example.sh and added docs
    Previously, local_setup.sh was responsible for setting
    the BIN environment variable.  To increase flexibility,
    maintainability, and robustness, spec scripts should
    not rely on BIN in the future but rather rely on PATH
    and RUBYLIB being set properly to the version of puppet.
    
    Signed-off-by: Jeff McCune <jeff@puppetlabs.com>

--- local_setup.example.sh
+++ local_setup.example.sh
@@ -1,2 +1,6 @@
-cd ~/puppet/lib
-BIN=../bin
+# JJM Set the base directory where puppet, facter, etc are checked out.
+: ${FACTER_PUPPET_BASE:=/opt/puppetlabs}
+# JJM Set the RUBYLIB
+export RUBYLIB="${FACTER_PUPPET_BASE}/puppet/lib:${FACTER_PUPPET_BASE}/facter/lib:${FACTER_PUPPET_BASE}/puppet-scaffold/lib:${FACTER_PUPPET_BASE}/puppet-module-tool/lib:/Users/jeff/customization/lib/ruby"
+# JJM Set the PATH
+export PATH="${FACTER_PUPPET_BASE}/puppet/sbin:${FACTER_PUPPET_BASE}/puppet/bin:${FACTER_PUPPET_BASE}/facter/bin:${FACTER_PUPPET_BASE}/puppet-scaffold/bin:${PATH}"
