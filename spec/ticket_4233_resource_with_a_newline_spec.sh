cb5867839ef31f84f8ad784b5af05171982f7bf6
Jeff McCune <jeff@puppetlabs.com>
2010-07-22 11:01:42 -0700

    Added grep to find ^err: for test 4233
    The spec test wasn't properly failing as it should
    when running with puppet 2.6.0rc2 and 2.6.0rc3.
    
    We cannot rely on the exit status of puppet to detect
    failures, we should instead look at the output for
    the error message reported in the issue ticket.
    
    Signed-off-by: Jeff McCune <jeff@puppetlabs.com>

--- spec/ticket_4233_resource_with_a_newline_spec.sh
+++ spec/ticket_4233_resource_with_a_newline_spec.sh
@@ -5,11 +5,16 @@ set -u
 
 source spec/setup.sh
 
-execute_manifest <<'PP'
+# JJM We expect 2.6.0rc3 to return an error
+# and 2.6.0 final to not return an error line.
+# Look for the line in the output and fail the test
+# if we find it.
+execute_manifest false <<'PP' | grep ^err:
 exec { '/bin/echo -e "\nHello World\n"': }
 PP
+# rval is the exit status of grep
 rval=$?
 
-# JJM Exit with a proper failure code if puppet didn't run
-test $rval -eq 0 && exit $EXIT_OK || exit $EXIT_FAILURE
+# JJM Exit with a proper failure code if the line was found
+test $rval -eq 0 && exit $EXIT_FAILURE || exit $EXIT_OK
 
