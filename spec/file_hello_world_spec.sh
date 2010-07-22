e253da186687dad68a385af622eda4de5adc306d
Jeff McCune <jeff@puppetlabs.com>
2010-07-22 15:12:52 -0700

    Updated file_hello_world to exit $EXIT_FAILURE
    Signed-off-by: Jeff McCune <jeff@puppetlabs.com>

--- spec/file_hello_world_spec.sh
+++ spec/file_hello_world_spec.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+set -u
 source spec/setup.sh
+set -e
 
 execute_manifest <<PP
 file{'/tmp/hello.$$.txt': content => 'hello world'}
 PP
 
-grep 'hello world' /tmp/hello.$$.txt
+if grep -q 'hello world' /tmp/hello.$$.txt; then
+  exit $EXIT_OK
+else
+  exit $EXIT_FAILURE
+fi
+
