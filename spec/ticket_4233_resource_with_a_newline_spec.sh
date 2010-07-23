d798a9a1e4976d63b3480ff45b9f082e9bfe1f20
Jeff McCune <jeff@puppetlabs.com>
2010-07-22 17:56:33 -0700

    Verified [#4233]
    Signed-off-by: Jeff McCune <jeff@puppetlabs.com>

--- spec/ticket_4233_resource_with_a_newline_spec.sh
+++ spec/ticket_4233_resource_with_a_newline_spec.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+#
+# 2010-07-22 Jeff McCune <jeff@puppetlabs.com>
+#
+# AffectedVersion: 2.6.0rc3
+# FixedVersion: 2.6.0
 
 set -u
 
@@ -8,12 +13,16 @@ source spec/setup.sh
 # and 2.6.0 final to not return an error line.
 # Look for the line in the output and fail the test
 # if we find it.
-execute_manifest <<'PP' | grep ^err:
+execute_manifest <<'PP' | grep '^err:'
 exec { '/bin/echo -e "\nHello World\n"': }
 PP
 # rval is the exit status of grep
 rval=$?
 
 # JJM Exit with a proper failure code if the line was found
-test $rval -eq 0 && exit $EXIT_FAILURE || exit $EXIT_OK
+if [ $rval -eq 0 ]
+  exit $EXIT_FAILURE
+else
+  exit $EXIT_OK
+fi
 
