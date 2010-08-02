a5a92b495d37c0ead9e019b1326e69f36b46d1ec
Nan Liu <nan@puppetlabs.com>
2010-08-02 16:05:07 -0700

    Cleanup script.

--- spec/ticket_4285_file_resource_fail_when_name_defined_instead_of_path_spec.sh
+++ spec/ticket_4285_file_resource_fail_when_name_defined_instead_of_path_spec.sh
@@ -10,19 +10,19 @@ source lib/setup.sh
 
 
 # NL: Expect 2.6.0rc4 to return an error:
-# Puppet::Parser::AST::Resource failed with error ArgumentError: 
-# Cannot alias File[file2] to [nil]; resource ["File", [nil]] already exists... 
-# Bug fixd by 2.6.0 
+# Puppet::Parser::AST::Resource failed with error ArgumentError:
+# Cannot alias File[file2] to [nil]; resource ["File", [nil]] already exists...
+# Bug fixd by 2.6.0
 if execute_manifest <<'EOF'
 file { "file1":
     name => '/tmp/file1',
     source => "/tmp/",
-}   
+}
 
 file { "file2":
     name => '/tmp/file2',
     source => "/tmp/",
-} 
+}
 EOF
 then
   exit $EXIT_OK
