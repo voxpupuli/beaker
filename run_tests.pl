dcfdb702502c1df3c882e92daba0348700f3e1d1
chromatic <chromatic@wgz.org>
2010-11-05 07:08:23 +0800

    Improved test runner.
    This version should be slightly more correct.  It also produces a relevant exit
    code based on the presence of any unknown or failing tests.

--- run_tests.pl
+++ run_tests.pl
@@ -18,13 +18,12 @@ my %report_test =
     11 => \&report_not_applicable,
 );
 
-main(@ARGV);
-exit;
+exit main(@ARGV);
 
 sub main
 {
     my @tests = find_tests();
-    run_tests( @tests );
+    return run_tests( @tests );
 }
 
 sub find_tests
@@ -34,11 +33,16 @@ sub find_tests
 
 sub run_tests
 {
+    my $seen_failure = 0;
+
     for my $test (@_)
     {
-        my $xml = run_one_test( $test );
+        my ($xml, $status) = run_one_test( $test );
         write_xml( $test, $xml );
+        $seen_failure++ if $status;
     }
+
+    return $seen_failure;
 }
 
 sub run_one_test
@@ -103,7 +107,9 @@ sub report_failure
   </testcase>
 TESTCASE
     $xml .= create_xml_footer( stdout => $out, stderr => $err );
-    return $xml;
+
+    # this one reports a failure
+    return( $xml, 1 );
 }
 
 sub report_not_applicable
@@ -113,12 +119,13 @@ sub report_not_applicable
 
 sub report_unknown_status
 {
-    return report_ok( @_, errors => 1 );
+    # this one also reports a failure
+    return( report_ok( @_, errors => 1 ), 1 );
 }
 
 sub create_xml_header
 {
-    my %args = 
+    my %args =
     (
         errors   => 0,
         failures => 0,
@@ -126,8 +133,8 @@ sub create_xml_header
         tests    => 1,
         skipped  => 0,
         time     => '0.00',
-        @_;
-    )
+        @_,
+    );
 
 return <<"XML";
 <?xml version="1.0" encoding="UTF-8"?>
@@ -153,8 +160,8 @@ sub write_xml
 {
     my ($test, $xml) = @_;
 
-    $test =~ s/spec/results/;
-    $test =~ s/.sh/.xml/;
+    $test =~ s[spec/][results/];
+    $test =~ s/.sh$/.xml/;
 
     open my $out, '>', $test or die "Cannot write '$test': $!\n";
     print {$out} $xml;
@@ -165,8 +172,8 @@ sub filename_to_test_name
 {
     my $filename = shift;
 
-    $filename =~ s/^spec.(\w+)_spec.sh$/$1/;
-    $filename =~ tr/_/ /g;
+    $filename =~ s/^spec.(\w+)_spec.sh/$1/;
+    $filename =~ tr/_/ /;
 
     return $filename;
 }
