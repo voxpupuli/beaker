986a504e879b77579072e68f09c5c16fc83e4973
Dominic Maraglia <dmaraglia@gmail.com>
2010-11-12 11:45:59 -0800

    Mod q_ files fir q_install=

--- tests1/02_InstallPuppet.rb
+++ tests1/02_InstallPuppet.rb
@@ -17,15 +17,13 @@ class InstallPuppet
       if /^PMASTER/ =~ os then         # Detect Puppet Master node
         BeginTest.new(host, test_name)
         runner = RemoteExec.new(host)
-        result = runner.do_remote("#{inst_path}/puppet-enterprise-installer -a #{inst_path}/answers_master_only.sh")
-        #result = runner.do_remote("uname -a")
+        result = runner.do_remote("env q_puppetclient_certname=`hostname` #{inst_path}/puppet-enterprise-installer -a #{inst_path}/q_master_dashboard.sh")
         @fail_flag+=result.exit_code
         ChkResult.new(host, test_name, result.stdout, result.stderr, result.exit_code)
       elsif /^AGENT/ =~ os then        # Detect Puppet Agent node
         BeginTest.new(host, test_name)
         runner = RemoteExec.new(host)
-        result = runner.do_remote("#{inst_path}/puppet-enterprise-installer -a #{inst_path}/answers_client_only.sh")
-        #result = runner.do_remote("/bin/ls -l /var")
+        result = runner.do_remote("env q_puppetclient_certname=`hostname` #{inst_path}/puppet-enterprise-installer -a #{inst_path}/q_agent_only.sh")
         @fail_flag+=result.exit_code
         ChkResult.new(host, test_name, result.stdout, result.stderr, result.exit_code)
       end
