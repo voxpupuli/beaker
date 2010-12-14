02a0c25fdb017c8825d9f5fb5889107c6928802d
Daniel Pittman <daniel@rimspace.net>
2010-12-15 10:32:32 +1100

    Support sending STDIN data in RemoteExec.
    Extend the remote execution class to be able to supply a static set of data as
    input on STDIN of the remote command (or shell).  This supports our ability to
    test puppet manifests remotely without extra temporary files.

--- lib/RemoteExec.rb
+++ lib/RemoteExec.rb
@@ -4,16 +4,27 @@
 require 'lib/action'
 
 class RemoteExec < Action
-  def do_remote(cmd)	
+  def do_remote(cmd, stdin = '')
     do_action(cmd) { |result,options|
       Net::SSH.start(host, "root", options) do |ssh|
         ssh.open_channel do |channel|
           channel.exec(cmd) do |ch, success|
             abort "FAILED: couldn't execute command (ssh.channel.exec failure)" unless success
-          end 
-          channel.on_data { |ch, data| result.stdout << data }
-          channel.on_extended_data { |ch, type, data| result.stderr << data if type == 1 }
-          channel.on_request("exit-status") { |ch, data| result.exit_code = data.read_long }
+
+            ch.on_data { |ch, data| result.stdout << data }
+            ch.on_extended_data { |ch, type, data| result.stderr << data if type == 1 }
+            ch.on_request("exit-status") { |ch, data|
+              result.exit_code = data.read_long
+            }
+
+            # queue stdin data, force it to packets, and signal eof: this
+            # triggers action in many remote commands, notably including
+            # 'puppet apply'.  It must be sent at some point before the rest
+            # of the action.
+            ch.send_data(stdin.to_s)
+            ch.process
+            ch.eof!
+          end
         end
       end
     }
