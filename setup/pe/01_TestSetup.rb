19c919932e3de14f3bfc01d66d5db3db711e62d7
Daniel Pittman <daniel@rimspace.net>
2011-01-07 15:57:57 -0800

    Feature #5519 -- improve platform identification in tests
    As part of the improvements to testing we identified some redundant code in
    the PE installer script.  This eliminates and unifies that, making the process
    much cleaner.

--- setup/pe/01_TestSetup.rb
+++ setup/pe/01_TestSetup.rb
@@ -4,31 +4,30 @@
 test_name "Install puppet and facter on target machines..."
 
 hosts.each do |host|
+  version  = host["puppetver"]
+  dist_dir = "puppet-enterprise-#{version}-#{host['platform']}"
+  dist_tar = "puppet-enterprise-#{version}-#{host['platform']}.tar"
+
+  unless File.file? dist_tar
+    puts "PE #{dist_tar} not found, help!"
+    puts ""
+    puts "Make sure your configuration file uses the PE version string:"
+    puts "  eg: rhel-5-x86_64  centos-5-x86_64"
+    fail_test "Sorry, PE #{dist_tar} file not found."
+  end
+
   step "Pre Test Setup -- SCP install package to hosts"
-  version = host["puppetver"]
-  dist_tar = case host['platform']
-             when /RHEL5-64/; "puppet-enterprise-#{version}-rhel-5-x86_64.tar"
-             when /CENT5-64/; "puppet-enterprise-#{version}-centos-5-x86_64.tar"
-             else fail "Unknown platform: #{host['platform']}"
-             end
   scp_to host, "#{$work_dir}/tarballs/#{dist_tar}", "/root"
   scp_to host, "#{$work_dir}/tarballs/answers.tar", "/root"
 
   step "Pre Test Setup -- Untar install package on hosts"
   on host,"tar xf #{dist_tar}"
 
+  step "Pre Test Setup -- Install the installer answers"
   version        = host["puppetver"]
   role_agent     = host['roles'].include? 'agent'
   role_master    = host['roles'].include? 'master'
   role_dashboard = host['roles'].include? 'dashboard'
-
-  # What platform is this host?
-  dist_dir = case host['platform']
-             when 'RHEL5-64' then "puppet-enterprise-#{version}-rhel-5-x86_64"
-             when 'CENT5-64' then "puppet-enterprise-#{version}-centos-5-x86_64"
-             else fail_test "unable to determine the platform " +
-                 "PE version for #{host['platform']}"
-             end
 
   q_script = case
              when (role_agent  && !role_dashboard); "q_agent_only.sh"
@@ -36,5 +35,7 @@ hosts.each do |host|
              when (role_master &&  role_dashboard); "q_master_and_dashboard.sh"
              else fail "#{host} has an unacceptable combination of roles."
              end
+
+  step "Pre Test Setup -- Run the Installer"
   on host,"cd #{dist_dir} && tar xf /root/answers.tar -C . && ./puppet-enterprise-installer -a #{q_script}"
 end
