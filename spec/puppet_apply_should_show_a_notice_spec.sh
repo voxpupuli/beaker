060b86c4d187d50a01dc0332ca53a15ff6c8e58b
Nick Lewis <nick@puppetlabs.com>
2011-02-11 11:12:41 -0800

    maint: Remove unused cruft from the old acceptance suite
    Removed all specs that have been converted to the new format, as well as
    driver/util files used to run them. Left specs which have yet to be converted.
    
    Reviewed-By: Matt Robinson

--- spec/puppet_apply_should_show_a_notice_spec.sh
+++
@@ -1,3 +0,0 @@
-source lib/setup.sh
-
-echo "notice 'Hello World'" | puppet apply | grep 'notice:.*Hello World'
--- spec/ticket_3172_puppet_kick_with_hostnames_on_the_command_line.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source lib/setup.sh
-        
-puppet kick hostname1 | tee /tmp/puppet-kick-$$.txt
-
-grep "Triggering hostname1" /tmp/puppet-kick-$$.txt
