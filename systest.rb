e9d3ec44baa956a3cd7b32828d6bb7efda55143c
Matt Robinson <matt@puppetlabs.com>
2011-02-11 11:12:59 -0800

    Extract code to methods to make systest easier to read
    For now in the same file so the commit is easier to read.  Will move
    those methods into other files in the next commit.

--- systest.rb
+++ systest.rb
@@ -30,70 +30,65 @@ end
 ###################################
 start_time = Time.new
 org_stdout = $stdout      # save stdout file descriptor
-test_summary={}           # hash to save test results
 
 options=parse_args
-if options[:tests].length < 1 then options[:tests] << 'tests' end
+setup_logs(start_time, options) unless options[:stdout_only]
 
-puts "Executing tests in #{options[:tests].join(', ')}"
-if options[:config]
-  puts "Using Config #{options[:config]}"
-else
-  fail "Argh!  There is no default for Config, specify one!"
+def read_config(options)
+  config = YAML.load(File.read(File.join($work_dir,options[:config])))
+
+  # Merge our default SSH options into the configuration.
+  ssh = {
+    :config                => false,
+    :paranoid              => false,
+    :auth_methods          => ["publickey"],
+    :keys                  => ["#{ENV['HOME']}/.ssh/id_rsa"],
+    :port                  => 22,
+    :user_known_hosts_file => "#{ENV['HOME']}/.ssh/known_hosts"
+  }
+  ssh.merge! config['CONFIG']['ssh'] if config['CONFIG']['ssh']
+  config['CONFIG']['ssh'] = ssh
+  config["CONFIG"]["puppetver"]=puppet_version
+  config
 end
+config = read_config(options)
 
-# Setup logging
-if ! options[:stdout_only] then
-  log_file = setup_logs(start_time, options)
-end
-
-# Read config file
-config = YAML.load(File.read(File.join($work_dir,options[:config])))
-
-# Merge our default SSH options into the configuration.
-ssh = {
-  :config                => false,
-  :paranoid              => false,
-  :auth_methods          => ["publickey"],
-  :keys                  => ["#{ENV['HOME']}/.ssh/id_rsa"],
-  :port                  => 22,
-  :user_known_hosts_file => "#{ENV['HOME']}/.ssh/known_hosts"
-}
-ssh.merge! config['CONFIG']['ssh'] if config['CONFIG']['ssh']
-config['CONFIG']['ssh'] = ssh
-
-# Generate installer answers files based on config
 gen_answer_files(config)
-
-# Add Puppet version to config
-config["CONFIG"]["puppetver"]=puppet_version
 
 if options[:mrpropper] 
   prepper = TestWrapper.new(config)
   prepper.clean_hosts(config) if options[:mrpropper]  # Clean-up old install
 end
 
-
-puts '=' * 78, "Performing test setup steps", ''
-# PE signs certs during install; after/ValidateSignCert is not required.
-# Not all test passes should exec after/*.  Need to another technique
-# for post installer steps.
-# ["setup/early", "setup/#{options[:type]}", "setup/late"].each do |root|
-["setup/early", "setup/#{options[:type]}"].each do |root|
-  run_tests_under(config, options, root).each do |test, result|
-    unless result == 0 then
-      puts "Warn: Setup action #{test} returned non-zero"
-      # Installer often returns non-zero upon sucessful install and hence we should warn
-      # vs bailing at this stage.
-      # exit 1
+def perform_test_setup_steps(options, config)
+  puts '=' * 78, "Performing test setup steps", ''
+  # PE signs certs during install; after/ValidateSignCert is not required.
+  # Not all test passes should exec after/*.  Need to another technique
+  # for post installer steps.
+  # ["setup/early", "setup/#{options[:type]}", "setup/late"].each do |root|
+  ["setup/early", "setup/#{options[:type]}"].each do |root|
+    run_tests_under(config, options, root).each do |test, result|
+      unless result == 0 then
+        puts "Warn: Setup action #{test} returned non-zero"
+        # Installer often returns non-zero upon sucessful install and hence we should warn
+        # vs bailing at this stage.
+        # exit 1
+      end
     end
   end
 end
 
-options[:tests].each do |root|
-  puts nil, '=' * 78, nil, "Running tests from #{root}"
-  test_summary.merge! run_tests_under(config, options, root)
+perform_test_setup_steps(options, config)
+
+def run_the_tests(options, config)
+  test_summary={}           # hash to save test results
+  options[:tests].each do |root|
+    puts nil, '=' * 78, nil, "Running tests from #{root}"
+    test_summary.merge! run_tests_under(config, options, root)
+  end
+  test_summary
 end
+test_summary = run_the_tests(options, config)
 
 # Dump summary of test results
 summarize(test_summary, start_time, config, options[:stdout])
