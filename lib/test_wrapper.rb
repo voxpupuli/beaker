44b0a7e3c2aba7836bfefdeb468ac9fc391954ee
Jacob Helwig <jacob@technosorcery.net>
2011-03-08 10:47:45 -0800

    Add helper for creating remote files
    create_remote_file can be used to easily do the dance of creating a
    tempfile, and scp'ing it to a remote host, in order to generate a remote
    file, instead of having to do echo & redirection magic via "on".
    
    Paired-with: Jesse Wolfe <jesse@puppetlabs.com>

--- lib/test_wrapper.rb
+++ lib/test_wrapper.rb
@@ -1,6 +1,7 @@
 class TestWrapper
   require 'lib/test_wrapper/host'
   require 'lib/gen_answer_files'
+  require 'tempfile'
 
   include GenAnswerFiles
 
@@ -192,6 +193,13 @@ class TestWrapper
     end
   end
 
+  def create_remote_file(hosts, file_path, file_content)
+    Tempfile.open 'puppet-acceptance' do |tempfile|
+      File.open(tempfile.path, 'w') { |file| file.puts file_content }
+
+      scp_to hosts, tempfile.path, file_path
+    end
+  end
 
   def get_git_sha1
   # config["CONFIG"]["puppet_ver"]
