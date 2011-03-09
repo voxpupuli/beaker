cc8e33e3b4fb75932f7aab914f6943a1360ddc96
Dominic Maraglia <dmaraglia@gmail.com>
2011-03-09 23:49:14 +0000

    Check point for libvert intregration into harness
    Communication with esx server complte
    Retreival of hosts and esx domains complete

--- lib/vmrun.rb
+++ lib/vmrun.rb
@@ -1,49 +1,44 @@
 module VmManage
   def vmrun(config)
+    vminfo_h = Hash.new
+    PTY.spawn("/opt/libvirt/bin/virsh -c esx://root@soko/?no_verify=1") do |r_f,w_f,pid|
+      w_f.sync = true
+      $expect_verbose = false
 
-  vms = []
-  PTY.spawn("/opt/libvirt/bin/virsh -c esx://root@soko/?no_verify=1") do |r_f,w_f,pid|
-    w_f.sync = true
-    $expect_verbose = false
+      # Login
+      r_f.expect(/^Enter root's password for soko: /) do
+        w_f.print "Puppetmaster!\n"
+      end
 
-    # Login
-    r_f.expect(/^Enter root's password for soko: /) do
-      w_f.print "Puppetmaster!\n"
-    end
+      # Send command
+      r_f.expect("virsh # ") do
+        w_f.print "list\n"
+      end
 
-    # Send command
-    r_f.expect("virsh # ") do
-      w_f.print "list\n"
-    end
-
-    # Parse output for VM instances
-    r_f.expect("virsh # ") do |output|
-      for x in output[0].split("\n")
-        if x =~ /\s(\w.*centos.*)\s.*\w/ then
-          vms.push $1
+      # Parse output for VM instances
+      r_f.expect("virsh # ") do |output|
+        for x in output[0].split("\n")
+          hosts.each do |host|  # only add lines that match a hostname
+            if x.index(host)
+              # VM list is returned as:
+              # 16 hostname vm_state
+              if x =~ /\s(\d+\s\S+)\s/ then
+                k,v = x.split(" ")
+                vminfo_h[v]=k
+              end
+            end
+          end
         end
       end
-    end
-    begin
-      w_f.print "quit\n"
-    rescue
-    end
-  end
+      begin
+        w_f.print "quit\n"
+      rescue
+      end
+    end  # spawn
 
-  vminfo_h = Hash.new
-  puts "Found VMs : \n"
-  vms.each do |line|
-    k,v = line.split(" ")
-    vminfo_h[v]=k
-  end
-
-  vminfo_h.each { |key, val|
-    puts "KEY: #{key}  VAL: #{val}"
-  }
-
-hosts.each do |host|
-  puts host
-end
-
+    puts "Found VMs : \n"
+    vminfo_h.each { |key, val|
+      puts "KEY: #{key}  VAL: #{val}"
+    }
   end
 end
