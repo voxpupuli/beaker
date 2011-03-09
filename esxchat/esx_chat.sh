9a2c497e6c341cdcf12be722d7913f33fa97cee1
Dominic Maraglia <dmaraglia@gmail.com>
2011-03-09 11:51:05 -0800

    Added base working libvert interfaces
    esx_chat.sh: Bash/expect example interface
    
    rubyesx.rb: ruby version of livert interface; this is what
    will be integrated into the test harness

--- esxchat/esx_chat.sh
+++ esxchat/esx_chat.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 PASS="Puppetmaster!"
 
-expect -c "spawn /opt/libvirt/bin/virsh -c esx://root@soko/?no_verify=1 list"
-expect "\"Enter root's password for soko: \""
-send "\"$PASS\r\""
-result = expect "\"virsh # \""
-
-echo "$result"
-
-exit 0
-
 result=$(expect -c "
 spawn /opt/libvirt/bin/virsh -c esx://root@soko/?no_verify=1 list
 expect \"Enter root's password for soko: \"
@@ -17,5 +8,3 @@ send \"$PASS\r\"
 expect \"virsh # \"
 ")
 echo "$result"
-
-#expect \"root@tb-driver:~# \"
