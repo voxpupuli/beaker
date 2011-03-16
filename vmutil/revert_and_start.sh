9ba31a0593fc99bc4aa12257190fb922b0fcd460
Dominic Maraglia <dmaraglia@gmail.com>
2011-03-16 12:08:23 -0700

    Added code for better VM mgmnt, non-pe cert tests
    Moved non PE cert signing to git installer area
    
    Added additional functionality to vm tools for reverting.

--- vmutil/revert_and_start.sh
+++ vmutil/revert_and_start.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
 
+usage()
+{
+  echo "revert_and_start.sh VM_image_file Snapshot_name"
+  exit 1
+}
+
 if [ -z $1 ]; then
   echo "VM image file list missing!"
-  exit 1
+  usage
+fi
+
+if [ -z $2 ]; then
+  echo "Snapshot name missing!"
+  usage
 fi
 
 # Revert
-./vmmanage.sh -i $1 -v snap1
+./vmmanage.sh -i $1 -v $2
 sleep 3
 
 # Start
