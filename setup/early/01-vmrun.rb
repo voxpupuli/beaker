c7d5b4c55d416d1d93089d17bd214885f78a381e
Dominic Maraglia <dmaraglia@gmail.com>
2011-03-18 13:54:34 -0700

    Revert "(#6733) Nicer integration of virsh to the harness."
    This reverts commit bd93c715c5376798994b49965d83f392cad1b99f.
    
    Conflicts:
    
    	setup/early/01-vmrun.rb

--- setup/early/01-vmrun.rb
+++ setup/early/01-vmrun.rb
@@ -1,30 +1,14 @@
-if options[:vmrun] = options[:type]
-  require 'timeout'
-
-  def virsh(*args)
-    secret = '/etc/plharness/secret'
-    uri    = 'esx://root@soko/?no_verify=1'
-    cmd    = args.join(' ')
-    output = `virsh -c #{uri} #{cmd} < #{secret}`
-
-    # Meh.  This is ugly but useful. --daniel 2011-03-16
-    output.sub!(/Enter root's password for soko: /, '')
-
-    puts "virsh> #{cmd}"
-    output.each { |line| puts "virsh< #{line}" }
-
-    return output
-  end
-
+if options[:vmrun]
   step "Reverting and starting VMs"
 
   vminfo_h = Hash.new
 
   # get list of VMs
-  hlist = virsh 'list'
+  hlist=`lib/virsh_exec.exp list`
 
   # interate through the VMs...
-  hlist.each do |line|
+  hlist.split("\n").each do |line|
+    Log.debug("VM: considering '#{line}'")
     hosts.each do |host|  # only add VMs that match a hostname
       if line.index(host)
         if line =~ /\s(\d+\s\S+)\s/ then
@@ -38,6 +22,6 @@ if options[:vmrun] = options[:type]
   # Revert the VMs
   vminfo_h.each do |key, val|
     step "Reverting VM: #{key} on ESX domain #{val}"
-    virsh 'snapshot-revert', val, 'git'
+    system("lib/virsh_exec.exp snapshot-revert #{val} git")
   end
 end
