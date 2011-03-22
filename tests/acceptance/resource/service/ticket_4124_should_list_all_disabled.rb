7fd1b1213ddd2a512e795be636766c822a43726c
Dominic Maraglia <dmaraglia@gmail.com>
2011-03-22 13:00:04 -0700

    Few fixes to improve services tests
    Cleaned up the code to be easier to read.  Both these tests are
    fairly fraglile, but a false fail is better than a false pass.
    
    TODO: improve services tests

--- tests/acceptance/resource/service/ticket_4124_should_list_all_disabled.rb
+++ tests/acceptance/resource/service/ticket_4124_should_list_all_disabled.rb
@@ -1,33 +1,15 @@
-test_name "#4124: should list all disabled services"
-pass_test "Pass forced pending test failure investigation"
-fail_test "this was 'pending' in the acceptance suite"
+test_name "#4124: should list all disabled services on Redhat/CentOS"
 
-%q^
+step "Validate disabled services agreement ralsh vs. OS service count"
 
-#!/bin/bash
+# This will remotely exec:
+# ticket_4124_should_list_all_disabled.sh
+# and parse the results
 
-set -e
-set -u
+rexec_dir="/opt/puppet-git-repos/puppet-acceptance/remote_exec"
 
-source lib/setup.sh
-RALSH_FILE=/tmp/ralsh-disabled-list-$$
-SERVICE_FILE=/tmp/service-disabled-list-$$
-# collect all service namevars 
-puppet resource service | egrep -B2 "enable\s*=>\s*'false" | grep "service {" | awk -F"'" '{print $2}' | sort  > $RALSH_FILE
-
-if [ -e $SERVICE_FILE ]; then
- rm $SERVICE_FILE
-fi
-SERVICEDIR='/etc/init.d'
-for SERVICE in $( ls $SERVICEDIR | sort | egrep -v "(functions|halt|killall|single|linuxconf)" ) ; do
-  if ! chkconfig $SERVICE; then
-    echo $SERVICE >> $SERVICE_FILE
-  fi
-done
-if diff $RALSH_FILE $SERVICE_FILE ; then
-  exit 0
-else
-  exit 1
-fi
-
-^
+hosts.each do |host|
+  if host['platform'].include? 'redhat' || 'centos'
+    on host,"#{rexec_dir}/ticket_4124_should_list_all_disabled.sh #{host['platform']}"
+  end
+end
