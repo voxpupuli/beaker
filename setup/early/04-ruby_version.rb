bd5a588ff79f42a68c68495813f148dfd565177c
Dominic Maraglia <dmaraglia@gmail.com>
2011-04-25 11:06:02 -0700

    Add code to allow ruby versions, fix test code
    Adding code to allow specification of test ruby version
    to use during test; need to test across multiple ruby versions
    and need to integrate this into the automated system.
    
    Fix: ticket_6928_puppet_master_parse_fails.rb
    The error output in Faces changed causing regexing of the
    error to fail; also, error was formerly issued on stderr
    and now is issued on stdout.

--- setup/early/04-ruby_version.rb
+++ setup/early/04-ruby_version.rb
@@ -1,8 +1,9 @@
-unless options[:rvm].include? 'skip'
+if options[:rvm].include? 'system'
+  step "Setting Ruby version to sytem default"
+  on hosts, "rvm --default system" 
+elsif options[:rvm].include? 'skip'
+  Log.notify "Skipping set ruby version"
+else 
   step "Setting Ruby version"
   on hosts, "rvm --default use #{options[:rvm]}"
-  step "Query Ruby Version"
-  on hosts, "ruby --version"
-else 
-  Log.notify "Skipping set ruby version"
 end
