68950f07388515d8e1ae3205542d708bd4be8f23
Dominic Maraglia <dominic@puppetlabs.com>
2011-06-03 13:39:11 -0700

    (#7193) Move acceptance tests to appropriate repo
    With tests now move to their appropriate repos, puppet-acceptance
    can be cleaned up.

--- tests/acceptance/puppet_apply_should_show_a_notice.rb
+++
@@ -1,5 +0,0 @@
-test_name "puppet apply should show a notice"
-apply_manifest_on(agents, "notice 'Hello World'") do
-    fail_test "the notice didn't show" unless
-        stdout =~ /notice: .*: Hello World/
-end
