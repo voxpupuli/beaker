2f591c92475e7708dacc433ebe01631eb89e63e8
Dominic Maraglia <dominic@puppetlabs.com>
2011-08-09 07:55:56 -0700

    (maint) add timeout to ntpdate step
    Ntpdate failures are causing acceptance test to fail; ntpdate
    has a 1 second timeout by default, increasing this to 5 secs to
    allow a long enough windows for the PL network.

--- setup/early/03-ntpdate.rb
+++ setup/early/03-ntpdate.rb
@@ -1,7 +1,7 @@
 unless options[:notimesync]
   test_name "Update system time sync"
   step "run ntpdate against NTP pool systems"
-  on hosts, "ntpdate us.pool.ntp.org"
+  on hosts, "ntpdate -t 5 us.pool.ntp.org"
 else
   Log.notify "Skipping ntp time sync"
 end
