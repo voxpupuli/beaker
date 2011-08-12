78bf3825ed80cda75e8826d7c51c80a94c525569
Dominic Maraglia <dominic@puppetlabs.com>
2011-08-12 15:35:55 -0700

    (Maint) add support for master/dashboard

--- setup/early/02-gen-answer-file.rb
+++ setup/early/02-gen-answer-file.rb
@@ -67,6 +67,34 @@ q_rubydevelopment_install='y'
 q_vendor_packages_install='y'
 ]
 
+master_dashboard_a= %q[
+q_install=y
+q_puppet_symlinks_install=y
+q_puppetagent_install=n
+q_puppetdashboard_install=n
+q_puppetmaster_certdnsnames=puppet:`uname | grep -i sunos > /dev/null && hostname || hostname -s`
+q_puppetmaster_certname=`uname | grep -i sunos > /dev/null && hostname || hostname -s`
+q_puppetmaster_dashboard_hostname=DASHBOARDHOST
+q_puppetmaster_dashboard_port=3000
+q_puppetmaster_install=y
+q_puppetmaster_use_dashboard_classifier=y
+q_puppetmaster_use_dashboard_reports=y
+q_rubydevelopment_install=y
+q_vendor_packages_install=y
+q_puppet_symlinks_install='n'
+q_puppetagent_install='n'
+q_puppetdashboard_database_install='y'
+q_puppetdashboard_database_name='dashboard'
+q_puppetdashboard_database_password='puppet'
+q_puppetdashboard_database_root_password='puppet'
+q_puppetdashboard_database_user='dashboard'
+q_puppetdashboard_httpd_port='3000'
+q_puppetdashboard_install='y'
+q_puppetmaster_install='n'
+q_rubydevelopment_install='y'
+q_vendor_packages_install='y'
+]
+
 test_name="Generate Puppet Enterprise answer files"
 puts "Skipping #{test_name}" unless ( options[:type] =~ /pe/ )
 
@@ -88,7 +116,7 @@ if ( options[:type] =~ /pe/ ) then
     role_dashboard=TRUE if host['roles'].include? 'dashboard'
     # Host is only a Dashboard
     if !role_agent && !role_master && role_dashboard then
-      step "host #{host} is dashboard only"
+      step "Generate Dashboard only answer file"
       File.open("tmp/q_dashboard_only", 'w') do |fh|
         dashboard_only_a.split(/\n/).each do |line|    # Insert Puppet master host name
           if line =~ /(q_puppetagent_server=)MASTER/ then
@@ -98,31 +126,48 @@ if ( options[:type] =~ /pe/ ) then
         end
       end
     end
-    step "Generate Agent only answer file"
-    File.open("tmp/q_agent_only", 'w') do |fh|
-      agent_only_a.split(/\n/).each do |line|    # Insert Puppet master host name
-        if line =~ /(q_puppetagent_server=)MASTER/ then
-          line = $1+master
+    if role_agent && !role_master && !role_dashboard then
+      step "Generate Agent only answer file"
+      File.open("tmp/q_agent_only", 'w') do |fh|
+        agent_only_a.split(/\n/).each do |line|    # Insert Puppet master host name
+          if line =~ /(q_puppetagent_server=)MASTER/ then
+            line = $1+master
+          end
+          fh.puts line
         end
-        fh.puts line
       end
     end
-    step "Generate Master only answer file"
-    File.open("tmp/q_master_only", 'w') do |fh|
-      master_only_a.split(/\n/).each do |line|
-        if line =~ /(q_puppetmaster_dashboard_hostname=)DASHBOARDHOST/ then
-          line = $1+dashboardhost
+    if !role_agent && role_master && !role_dashboard then
+      step "Generate Master only answer file"
+      File.open("tmp/q_master", 'w') do |fh|
+        master_only_a.split(/\n/).each do |line|
+          if line =~ /(q_puppetmaster_dashboard_hostname=)DASHBOARDHOST/ then
+            line = $1+dashboardhost
+          end
+          fh.puts line
         end
-        fh.puts line
       end
     end
-    # Host is Agent and Dashboard
     if role_agent && !role_master && role_dashboard then
-      step "host #{host} is agent and dashboard"
+      step "Generate Agent and Dashboard answer file"
       File.open("tmp/q_agent_and_dashboard", 'w') do |fh|
         agent_dashboard_a.split(/\n/).each do |line|
           if line =~ /(q_puppetagent_server=)MASTER/ then
             line = $1+master
+          end
+          fh.puts line
+        end
+      end
+    end
+    if !role_agent && role_master && role_dashboard then
+      step "Generate Master and Dashboard answer file"
+      File.open("tmp/q_master", 'w') do |fh|
+        master_dashboard_a.split(/\n/).each do |line|
+          if line =~ /(q_puppetagent_server=)MASTER/ then
+            line = $1+master
+          end
+          if line =~ /(q_puppetmaster_dashboard_hostname=)DASHBOARDHOST/ then
+            line = $1+dashboardhost
           end
           fh.puts line
         end
