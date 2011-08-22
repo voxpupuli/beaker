1a4a6766592534af0fe8470008d7bc6055799f75
Dominic Maraglia <dominic@puppetlabs.com>
2011-08-22 12:19:52 -0700

    (maint) add puppet agent --test to post install

---
+++ setup/pe/05_Agent_test.rb
@@ -0,0 +1,21 @@
+test_name "Agent --test post install"
+zzz=4
+step 'Running pupept agent --test on each agent'
+agents.each do |agent|
+  if agent['platform'].include?('solaris')
+    on(agent, '/usr/sbin/svcadm disable -s svc:/network/puppetagent:default')
+    sleep zzz
+    on(agent, puppet_agent("--test"), :acceptable_exit_codes => [0,2])
+    on(agent, '/usr/sbin/svcadm enable svc:/network/puppetagent:default')
+  elsif agent['platform'].include?('debian') or agent['platform'].include?('ubuntu')
+    on(agent, '/etc/init.d/pe-puppet-agent stop')
+    sleep zzz
+    on(agent, puppet_agent("--test"), :acceptable_exit_codes => [0,2])
+    on(agent, '/etc/init.d/pe-puppet-agent start')
+  else
+    on(agent, '/etc/init.d/pe-puppet stop')
+    sleep zzz
+    on(agent, puppet_agent("--test"), :acceptable_exit_codes => [0,2])
+    on(agent, '/etc/init.d/pe-puppet start')
+  end
+end
