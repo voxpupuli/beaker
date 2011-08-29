f5da299c7652d2b4914a44baff08a02747eb380b
Dominic Maraglia <dominic@puppetlabs.com>
2011-08-29 10:32:18 -0700

    (maint) add -w flag for solaris ntpdate

--- setup/early/03-ntpdate.rb
+++ setup/early/03-ntpdate.rb
@@ -1,7 +1,13 @@
 unless options[:notimesync]
   test_name "Update system time sync"
   step "run ntpdate against NTP pool systems"
-  on hosts, "ntpdate -t 5 us.pool.ntp.org"
+  hosts.each do |host|
+    if host['platform'].include? 'solaris'
+      on(host, "ntpdate -w us.pool.ntp.org")
+    else
+      on(host, "ntpdate -t 5 us.pool.ntp.org")
+    end
+  end
 else
   Log.notify "Skipping ntp time sync"
   skip_test "Skipping ntp time sync"
