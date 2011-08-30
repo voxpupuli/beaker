78906ce86484abc0547d178d9eddbc1c3c6ff154
Dominic Maraglia <dominic@puppetlabs.com>
2011-08-30 10:28:46 -0700

    (maint) add sleep for solaris ntpdate

--- setup/early/03-ntpdate.rb
+++ setup/early/03-ntpdate.rb
@@ -2,11 +2,8 @@ unless options[:notimesync]
   test_name "Update system time sync"
   step "run ntpdate against NTP pool systems"
   hosts.each do |host|
-    until on(host, "ping -c 1 us.pool.ntp.org || ping us.pool.ntp.org 16 1 5") do
-      sleep 1 
-    end
     if host['platform'].include? 'solaris'
-      on(host, "ntpdate -w us.pool.ntp.org")
+      on(host, "sleep 10 && ntpdate -w us.pool.ntp.org")
     else
       on(host, "ntpdate -t 5 us.pool.ntp.org")
     end
