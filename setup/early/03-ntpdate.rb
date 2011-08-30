35680d5fcbe529f493239c7c388626d9a32b3116
Dominic Maraglia <dominic@puppetlabs.com>
2011-08-29 17:10:25 -0700

    (maint) add a poll loop for solaris ntp timeout

--- setup/early/03-ntpdate.rb
+++ setup/early/03-ntpdate.rb
@@ -2,6 +2,9 @@ unless options[:notimesync]
   test_name "Update system time sync"
   step "run ntpdate against NTP pool systems"
   hosts.each do |host|
+    until on(host, "ping -c 1 us.pool.ntp.org || ping us.pool.ntp.org 16 1 5") do
+      sleep 1 
+    end
     if host['platform'].include? 'solaris'
       on(host, "ntpdate -w us.pool.ntp.org")
     else
