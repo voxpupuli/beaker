3eb4bd61f922da01eee5b72b0a5d41cbe88fa9e4
Dominic Maraglia <dominic@puppetlabs.com>
2011-09-14 13:43:27 -0700

    (maint) Add PE specific check for ruby
    Checking the version on ruby was blindly looking for Ruby in the
    search path, else in the puppet bin dir.  Made the test specific,
    checking specifically for PE or non-PE installs.

--- setup/post/01-ruby_version.rb
+++ setup/post/01-ruby_version.rb
@@ -5,8 +5,14 @@
 # Need to run this post install as PE hosts will not have ruby installed
 # until post install
 
+if options[:type] =~ /pe/ || options[:type] =~ /pe_ro/ then
+  cmd = "#{config['puppetbindir']}/ruby -v"
+else
+  cmd = 'ruby -v'
+end 
+
 hosts.each do |host|
-  on(host, "ruby -v || #{config['puppetbindir']}/ruby -v") do
+  on(host, "#{cmd}") do
    host[:ruby_ver] = stdout
   end
 end
