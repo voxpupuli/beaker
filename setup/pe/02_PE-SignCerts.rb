0712c7660d754b22596b42fbfd1447b46a8ae398
Dominic Maraglia <dominic@puppetlabs.com>
2011-10-13 14:16:24 -0700

    (maint) use the new method "puppet" vs "on"
    Use the new puppet method to call "sign" which improves
    portability to non-Linux OSes.

--- setup/pe/02_PE-SignCerts.rb
+++ setup/pe/02_PE-SignCerts.rb
@@ -4,7 +4,7 @@ step "PE: Puppet Master Sign all Requested Agent Certs"
 hosts.each do |host| 
   # Master auto signs its own cert on startup
   next if host['roles'].include? 'master'
-  on master,"puppet cert --sign #{host}" do
+  on master, puppet("cert --sign #{host}") do
     assert_no_match(/Could not call sign/, stdout, "Unable to sign cert for #{host}")
   end
 end
