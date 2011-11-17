7b79318267f924d97c65dcf0a59ffc7c985bb9c9
Michael Higgins <mhiggins@puppetlabs.com>
2011-11-17 12:09:06 -0800

    solaris machine isn't using dhcp, fixing test
    Test renews dhcp lease, but solaris vm isn't using dhcp anyway

--- setup/early/03-dhcp-renew.rb
+++ setup/early/03-dhcp-renew.rb
@@ -3,10 +3,11 @@ test_name "renew dhcpd lease so we don't hiccup in the tests"
 step "renew dhcpd with dhclient -r && dhclient or dhcpcd -n eth0"
 
 hosts.each do |host|
-  if host['platform'].include? 'sles'
-    on host, "dhcpcd -n eth0"
-  else
-    on host, "dhclient -r && dhclient"
-  end
+  next if host['platform'].include?('solaris')
+    if host['platform'].include? 'sles'
+      on host, "dhcpcd -n eth0"
+    else
+      on host, "dhclient -r && dhclient"
+    end
 end
 
