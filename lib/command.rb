9e6c71039980f6a200e28efe1302d3647a74662d
Josh Cooper <josh@puppetlabs.com>
2012-01-17 11:48:53 -0800

    (#11767) Make RemoteExec commands work on Windows
    Previously, the acceptance tests could not launch puppet and facter via
    SSH and Cygwin on the Windows side, due to Cygwin's bash shell
    attempting to invoke ruby.exe on `C:\Ruby187\bin\puppet` and failing,
    because that file contains Windows-style paths that confuse bash.
    
    This commit simply wraps the puppet and facter commands within a
    `cmd.exe` shell to escape from the bash shell (though the environment
    is inherited).

--- lib/command.rb
+++ lib/command.rb
@@ -19,7 +19,9 @@ class Command
   def puppet_env_command(host_info)
     rubylib = [host_info['pluginlibpath'], host_info['puppetlibdir'], host_info['facterlibdir'],'$RUBYLIB'].compact.join(':')
     path    = [host_info['puppetbindir'], host_info['facterbindir'],'$PATH'   ].compact.join(':')
-    %Q{env RUBYLIB="#{rubylib}" PATH="#{path}"}
+    cmd     = host_info['platform'] =~ /windows/ ? 'cmd.exe /c' : ''
+
+    %Q{env RUBYLIB="#{rubylib}" PATH="#{path}" #{cmd}}
   end
 end
 
