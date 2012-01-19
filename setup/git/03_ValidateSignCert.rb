4fdfbc2d7e484e96d86c4568c4d6541706e90155
Josh Cooper <josh@puppetlabs.com>
2012-01-19 15:29:02 -0800

    (#11767) Use newer --dns_alt_names option
    Previously, the git setup steps were using the old deprecated
    --certdnsnames option when generating the master's csr. It was also
    using a colon-delimited list instead of a comma-separated list. As a
    result, the puppetmasters certificate generated during
    03_ValidateSignCert.rb contained bogus X509 Subject Alt Names:
    
      X509v3 Subject Alternative Name:
        DNS:centos-55-64-1.puppetlabs.lan,
        DNS:puppet:centos-55-64-1:centos-55-64-1
    
    This was not noticed earlier, because the agent never retrieved its
    ssl certificate in the setup steps, which is something I recently
    changed so that we'd validate that the agent can connect via SSL to
    the master in the setup steps, rather than later during the acceptance
    tests.
    
    Previously, when running the first acceptance tests from the puppet
    repo, the test would delete all of the hosts ssl directories, and
    regenerate the certs, but this time using the correct --dns_alt_names
    option.
    
    This commit just updates the setup steps to also use the
    --dns_alt_names option with a comma delimited list of names to include
    in the puppet master's certificate.

--- setup/git/03_ValidateSignCert.rb
+++ setup/git/03_ValidateSignCert.rb
@@ -1,7 +1,7 @@
 test_name "Validate Sign Cert"
 
 step "Master: Start Puppet Master"
-with_master_running_on(master, "--certdnsnames=\"puppet:$(hostname -s):$(hostname -f)\" --verbose") do
+with_master_running_on(master, "--dns_alt_names=\"puppet,$(hostname -s),$(hostname -f)\" --verbose") do
   step "Agents: Run agent --test first time to gen CSR"
   on agents, puppet_agent("--test"), :acceptable_exit_codes => [1]
 
