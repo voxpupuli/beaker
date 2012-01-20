1bae816e0630e56823c3c40234d82bc6e0a9bb4f
Daniel Pittman <daniel@rimspace.net>
2012-01-20 12:30:24 -0800

    Support reuse of an existing git repository checkout.
    Previously the suite would delete the entire git source checkouts, then fetch
    them from scratch.  This would ensure that the content was clean, but wasn't
    actually terribly efficient since almost all content was identical each time.
    
    Now, we clone if the repository is missing, but otherwise update the `origin`
    remote to whatever we are targeted at, then fetch that.  This has the same net
    effect but avoids fetching 36+MB every time the suite runs.
    
    Signed-off-by: Daniel Pittman <daniel@rimspace.net>

--- setup/git/03_ValidateSignCert.rb
+++ setup/git/03_ValidateSignCert.rb
@@ -3,7 +3,7 @@ test_name "Validate Sign Cert"
 step "Master: Start Puppet Master"
 with_master_running_on(master, "--dns_alt_names=\"puppet,$(hostname -s),$(hostname -f)\" --verbose") do
   step "Agents: Run agent --test first time to gen CSR"
-  on agents, puppet_agent("--test"), :acceptable_exit_codes => [1]
+  on agents, puppet_agent("--test"), :acceptable_exit_codes => [0,1,2]
 
   # Sign all waiting certs
   step "Master: sign all certs"
