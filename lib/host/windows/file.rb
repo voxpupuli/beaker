130985992aae213637683de524f8f0376ec14b04
cprice <chris@puppetlabs.com>
2012-02-09 16:16:07 -0800

    Add "puppet-acceptance" to base tmp dir path
    This commit just changes the default "root" temp dir for acceptance
    tests from "/tmp" to "/tmp/puppet-acceptance". The goal here is to
    quarantine acceptance test temp files into one easily-identifiable
    directory that can be cleaned up.
    
    This would only affect tests that were calling the *::File.tmpdir
    methods, and should have no effect on the success or failure of any
    tests.

--- lib/host/windows/file.rb
+++ lib/host/windows/file.rb
@@ -2,14 +2,32 @@ module Windows::File
   include CommandFactory
 
   def tmpfile(name)
-    execute("cygpath -m $(mktemp -t #{name}.XXXXXX)")
+    execute("cygpath -m $(mktemp --tmpdir=#{base_tmpdir} -t #{name}.XXXXXX)")
   end
 
   def tmpdir(name)
-    execute("cygpath -m $(mktemp -td #{name}.XXXXXX)")
+    execute("cygpath -m $(mktemp --tmpdir=#{base_tmpdir} -td #{name}.XXXXXX)")
   end
 
   def path_split(paths)
     paths.split(';')
   end
+
+  # private utility method to get the base temp dir path; the goal here is to quarantine acceptance test temp files into
+  # one easily-identifiable directory that can be cleaned up easily
+  def base_tmpdir()
+    unless @base_tmp_dir then
+      @base_tmp_dir = "#{get_system_tmpdir}/puppet-acceptance"
+      mkdirs(@base_tmp_dir)
+    end
+    @base_tmp_dir
+  end
+  private :base_tmpdir
+
+  # private utility method for determining the system tempdir
+  def get_system_tmpdir()
+    # hard-coded for now, but this could execute a command on the system to check environment variables, etc.
+    "/tmp"
+  end
+  private :get_system_tmpdir
 end
