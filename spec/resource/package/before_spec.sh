23cda0c68f5bc4172e823736b76623298da03a1c
Justin Stoller <justin@puppetlabs.com>
2012-03-08 16:24:23 -0800

    someone left .sh tests around

--- spec/resource/package/before_spec.sh
+++
@@ -1,8 +0,0 @@
-#
-# sets up a local test repo used for yum tests
-#
-mkdir -p /var/yum
-deps_path="$( dirname $0 )/deps"
-cp -Rf $deps_path/repo /var/yum/
-cp $deps_path/local-puppet-spec.repo /etc/yum.repos.d/
-yum -d 0 -e 0 makecache
