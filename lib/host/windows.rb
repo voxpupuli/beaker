a30c1bbea870e51e8fbb59803a006b6993d4e986
Justin Stoller <justin@puppetlabs.com>
2012-03-22 15:19:52 -0700

    change the way puppet and facter are called to allow for '.bat' ext in PE Windows

--- lib/host/windows.rb
+++ lib/host/windows.rb
@@ -13,17 +13,29 @@ module Windows
     include Windows::File
     include Windows::Exec
 
+    PE_DEFAULTS = {
+      'user'         => 'Administrator',
+      'group'        => 'Administrators',
+      'puppetpath'   => '`cygpath -smF 35`/PuppetLabs/puppet/etc',
+      'puppetvardir' => '`cygpath -smF 35`/PuppetLabs/puppet/var',
+      'puppetbindir' => 'C:/PROGRA~1/PUPPET~1/PUPPET~1/bin',
+      'puppetbin'    => 'C:/PROGRA~1/PUPPET~1/PUPPET~1/bin/puppet.bat',
+      'facterbin'    => 'C:/PROGRA~1/PUPPET~1/PUPPET~1/bin/facter.bat'
+    }
+
     DEFAULTS = {
       'user'         => 'Administrator',
       'group'        => 'Administrators',
       'puppetpath'   => '`cygpath -smF 35`/PuppetLabs/puppet/etc',
-      'puppetvardir' => '`cygpath -smF 35`/PuppetLabs/puppet/var'
+      'puppetvardir' => '`cygpath -smF 35`/PuppetLabs/puppet/var',
+      'puppetbin'    => 'puppet',
+      'facterbin'    => 'facter'
     }
 
     def initialize(name, overrides, defaults)
       super(name, overrides, defaults)
 
-      @defaults = defaults.merge(DEFAULTS)
+      @defaults = defaults.merge(TestConfig.is_pe? ? PE_DEFAULTS : DEFAULTS)
     end
   end
 end
