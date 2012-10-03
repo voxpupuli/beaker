f2ca07071ce1ffe65c2798e04536111354821b16
Chris Price <chris@puppetlabs.com>
2012-10-03 13:05:18 -0700

    Add fqdn to /etc/hosts during startup
    In addition to the short hostname, some tests will definitely
    need for the /etc/hosts file to contain the FQDN.  (PuppetDB
    does for sure.)  This commit blatantly rips off some code
    from Facter to guess the domain name, and then adds it to
    the /etc/hosts file during VM runs.

--- setup/early/00-vmrun.rb
+++ setup/early/00-vmrun.rb
@@ -1,5 +1,25 @@
 test_name "Revert VMs"
 
+  # NOTE: this code is shamelessly stolen from facter's 'domain' fact, but
+  # we don't have access to facter at this point in the run.  Also, this
+  # utility method should perhaps be moved to a more central location in the
+  # framework.
+  def get_domain_name(host)
+    domain = nil
+    search = nil
+    on host, "cat /etc/resolv.conf"
+    resolv_conf = stdout
+    resolv_conf.each_line { |line|
+      if line =~ /^\s*domain\s+(\S+)/
+        domain = $1
+      elsif line =~ /^\s*search\s+(\S+)/
+        search = $1
+      end
+    }
+    return domain if domain
+    return search if search
+  end
+
   snap = options[:snapshot] || options[:type]
   snap = 'git' if snap == 'gem'  # Sweet, sweet consistency
   snap = 'git' if snap == 'manual'  # Sweet, sweet consistency
@@ -146,7 +166,8 @@ test_name "Revert VMs"
       on host, "hostname #{name}"
       on host, "ip a|awk '/g/{print$2}' | cut -d/ -f1 | head -1"
       ip = stdout.chomp
-      etc_hosts += "#{ip}\t#{name}\n"
+      domain = get_domain_name(host)
+      etc_hosts += "#{ip}\t#{name}\t#{name}.#{domain}\n"
     end
 
     # Send our hosts information to the nodes
