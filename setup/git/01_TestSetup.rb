da4e1c1694acedb67b6d76670787a6d67f988375
Justin Stoller <justin@puppetlabs.com>
2012-10-09 13:45:46 -0700

    pull out bug workarounds into separate file

--- setup/git/01_TestSetup.rb
+++ setup/git/01_TestSetup.rb
@@ -31,35 +31,12 @@ test_name "Install packages and repositories on target machines..." do
 
   config[:version] = versions
 
-  # Git based install assume puppet master named "puppet";
-  # create an puppet.conf file with server= entry
   step "Agents: create basic puppet.conf" do
     agents.each do |agent|
       puppetconf = File.join(agent['puppetpath'], 'puppet.conf')
 
       on agent, "echo '[agent]' > #{puppetconf} && " +
                 "echo server=#{master} >> #{puppetconf}"
-    end
-  end
-
-  # these workarounds should not be part of a standard install step
-  hosts.each do |host|
-    next if host['platform'].include? 'windows'
-
-    step "REVISIT: see #9862, this step should not be required for agents" do
-      on host, "getent group puppet || groupadd puppet"
-
-      if host['platform'].include? 'solaris'
-        useradd_opts = '-d /puppet -m -s /bin/sh -g puppet puppet'
-      else
-        useradd_opts = 'puppet -g puppet -G puppet'
-      end
-
-      on host, "getent passwd puppet || useradd #{useradd_opts}"
-    end
-
-    step "REVISIT: Work around bug #5794 not creating reports as required" do
-      on host, "mkdir -p /tmp/reports && chown puppet:puppet /tmp/reports"
     end
   end
 end
