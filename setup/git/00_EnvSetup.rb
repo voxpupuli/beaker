090284c93ed96741d97574bdf141dcde8aca2947
Josh Cooper <josh@puppetlabs.com>
2013-04-15 17:41:03 -0700

    (#11276) Add minitar for Windows
    This commit adds minitar so that the module tool will work on Windows.

--- setup/git/00_EnvSetup.rb
+++ setup/git/00_EnvSetup.rb
@@ -2,7 +2,7 @@ test_name "Setup environment"
 
 WINDOWS_GEMS = [
   'sys-admin -v1.5.6', 'win32console -v1.3.2', 'win32-security -v0.1.4', 'win32-dir -v0.3.7', 'win32-eventlog -v0.5.3',
-  'win32-process -v0.6.5', 'win32-service -v0.7.2', 'win32-taskscheduler -v0.2.2'
+  'win32-process -v0.6.5', 'win32-service -v0.7.2', 'win32-taskscheduler -v0.2.2', 'minitar -v0.5.4'
 ]
 
 hosts.each do |host|
