d4fc92e5bfdfdc078c8039f01c404448151e806a
Andrew Parker <andy@puppetlabs.com>
2013-04-29 13:45:05 -0700

    Move systest.rb to bin directory
    Without this change you could install the puppet-acceptance code from
    the git repo as a gem (via bundler), but could not run the systest.rb
    program. This moves systest.rb to the bin directory, leaves a stub
    behind, and drops the .rb extension (just 'cuz).

--- systest.rb
+++ systest.rb
@@ -1,22 +1,9 @@
 #!/usr/bin/env ruby
 
-require 'rubygems' unless defined?(Gem)
-require 'net/ssh'
-require 'net/scp'
-require 'net/http'
-require 'socket'
-require 'optparse'
-require 'systemu'
-require 'test/unit'
-require 'yaml'
-
-Dir[
-  File.expand_path(File.dirname(__FILE__)+'/lib/puppet_acceptance/*.rb')
-].each do |file|
-  require file
+if ENV['RUBYLIB']
+  ENV['RUBYLIB'] += ':lib'
+else
+  ENV['RUBYLIB'] = 'lib'
 end
 
-PuppetAcceptance::CLI.new.execute!
-
-puts "systest completed successfully, thanks."
-exit 0
+system('bin/systest', *ARGV)
