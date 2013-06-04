puppetlabs/puppet-acceptance

move ec2 install git+ruby from setup/early to lib

lib/puppet_acceptance/utils/vm_control.rb
@@ -241,6 +241,25 @@ module PuppetAcceptance
       end
     end #revert_fusion
 
+    def blimpy_install_git_and_ruby(blimpy_hosts)
+      @logger.notify "Install git and ruby on non-pe blimpy hosts"
+      blimpy_hosts.each do |host|
+
+        if host['platform'].include?('el-5')
+          host.exec(HostCommand.new("wget http://download3.fedora.redhat.com/pub/epel/5Server/i386/epel-release-5-4.noarch.rpm && rpm -ihv epel-release-5-4.noarch.rpm"))
+        end
+
+        if host['platform'].include?('el-')
+            host.exec(HostCommand.new("yum -y install git ruby"))
+        elsif host['platform'].include?('ubuntu') or host['platform'].include?('debian')
+            host.exec(HostCommand.new("apt-get -y install git-core ruby libopenssl-ruby"))
+        else
+          @logger.debug "Warn #{host['platform']} is not a supported platform, no packages will be installed."
+          next
+        end
+      end
+    end #blimpy_install_git_and_ruby
+
     def revert_blimpy(blimpy_hosts, snap, hosts)
       require 'rubygems' unless defined?(Gem)
       require 'blimpy'
@@ -310,6 +329,12 @@ module PuppetAcceptance
       # Send our hosts information to the nodes
       blimpy_hosts.each do |host|
         host.exec(HostCommand.new("echo '#{etc_hosts}' > /etc/hosts"))
+      end
+
+      #Install git and ruby if we are not pe
+      #HACK HACK HACK, type should not be in here
+      if config[:type] !~ /pe/
+        blimpy_install_git_and_ruby(blimpy_hosts)
       end
     end #revert_blimpy
 

voxpupuli/beaker

lib/puppet_acceptance/utils/vm_control.rb
@@ -237,6 +237,25 @@ module PuppetAcceptance
         end
       end #revert_fusion
 
+      def blimpy_install_git_and_ruby(blimpy_hosts)
+        @logger.notify "Install git and ruby on non-pe blimpy hosts"
+        blimpy_hosts.each do |host|
+
+          if host['platform'].include?('el-5')
+            host.exec(Command.new("wget http://download3.fedora.redhat.com/pub/epel/5Server/i386/epel-release-5-4.noarch.rpm && rpm -ihv epel-release-5-4.noarch.rpm"))
+          end
+
+          if host['platform'].include?('el-')
+              host.exec(Command.new("yum -y install git ruby"))
+          elsif host['platform'].include?('ubuntu') or host['platform'].include?('debian')
+              host.exec(Command.new("apt-get -y install git-core ruby libopenssl-ruby"))
+          else
+            @logger.debug "Warn #{host['platform']} is not a supported platform, no packages will be installed."
+            next
+          end
+        end
+      end #blimpy_install_git_and_ruby
+
       def revert_blimpy(blimpy_hosts, snap)
         require 'rubygems' unless defined?(Gem)
         require 'blimpy'
@@ -284,7 +303,7 @@ module PuppetAcceptance
               end
             rescue
             end
-            sleep rand(20)
+            sleep sleep_time
             retry
           else
             @logger.error("Retried Fog #{fleet_retries} times, giving up and throwing the exception")
@@ -309,6 +328,12 @@ module PuppetAcceptance
         # Send our hosts information to the nodes
         blimpy_hosts.each do |host|
           host.exec(Command.new("echo '#{etc_hosts}' > /etc/hosts"))
+        end
+
+        #Install git and ruby if we are not pe
+        #HACK HACK HACK, type should not be in here
+        if @config[:type] !~ /pe/
+          blimpy_install_git_and_ruby(blimpy_hosts)
         end
       end #revert_blimpy
 