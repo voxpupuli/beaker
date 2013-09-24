8751772a4c86c2f56f27e88f22e53c9dc55d4003
Josh Partlow <joshua.partlow@puppetlabs.com>
2013-09-23 17:09:44 -0700

    (maint) Correct solaris 11/10 package install/uninstall
    Solaris has different package handling between 11 and 10.  We were
    correct on install for Solaris 11, but uninstall was Solaris 10.
    
    Now differentiate between the two with the correct package handlers for
    11 and 10 respectively.
    
    This also corrects a bug checking for existing packages in Solaris 10.
    The which command returns an exit code of 0, even if the command is not
    found.  So we're testing stdout explicitly to see if a path to command
    was returned.

--- lib/beaker/host/unix/pkg.rb
+++ lib/beaker/host/unix/pkg.rb
@@ -3,7 +3,12 @@ module Unix::Pkg
 
   def check_for_package(name)
     result = exec(Beaker::Command.new("which #{name}"), :acceptable_exit_codes => (0...127))
-    result.exit_code == 0
+    case self['platform']
+    when /solaris-10/
+      result.stdout =~ %r|/.*/#{name}|
+    else  
+      result.exit_code == 0
+    end
   end
 
   def install_package(name, cmdline_args = '')
@@ -15,8 +20,10 @@ module Unix::Pkg
       when /ubuntu|debian/
         execute("apt-get update")
         execute("apt-get install #{cmdline_args} -y #{name}")
-      when /solaris/
+      when /solaris-11/
         execute("pkg #{cmdline_args} install #{name}")
+      when /solaris-10/
+        execute("pkgutil -i -y #{cmdline_args} #{name}")
       else
         raise "Package #{name} cannot be installed on #{self}"
     end
@@ -30,8 +37,10 @@ module Unix::Pkg
         execute("yum -y #{cmdline_args} remove #{name}")
       when /ubuntu|debian/
         execute("apt-get purge #{cmdline_args} -y #{name}")
-      when /solaris/
-        execute("pkgrm #{cmdline_args} #{name}")
+      when /solaris-11/
+        execute("pkg #{cmdline_args} uninstall #{name}")
+      when /solaris-10/
+        execute("pkgutil -r -y #{cmdline_args} #{name}")
       else
         raise "Package #{name} cannot be installed on #{self}"
     end
