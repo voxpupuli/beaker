adf10c49ba1f1d0e26b001f7eb69668e4f8b26a0
Alice Nodelman <alice@puppetlabs.com>
2013-10-28 10:51:37 -0700

    fixes in response to review comments

--- lib/beaker/network_manager.rb
+++ lib/beaker/network_manager.rb
@@ -16,6 +16,10 @@ module Beaker
       @hosts = []
       @virtual_machines = {}
       @noprovision_machines = []
+    end
+
+    def provision
+      #sort hosts into those to be provisioned and those to use non-provisioned
       @options['HOSTS'].each_key do |name|
         host_info = @options['HOSTS'][name]
         #check to see if this host has a hypervisor 
@@ -33,11 +37,8 @@ module Beaker
           @logger.debug "No hypervisor for #{name}, connecting to host without provisioning"
           @noprovision_machines << name
         end
-
       end
-    end
 
-    def provision
       @provisioned_set = {}
       @virtual_machines.each do |type, names|
         hosts_for_type = []
@@ -62,8 +63,12 @@ module Beaker
       @hosts.each {|host| host.close }
 
       if not @options[:preserve_hosts]
-        @provisioned_set.each_key do |type|
-          @provisioned_set[type].cleanup
+        if @provisioned_set
+          @provisioned_set.each_key do |type|
+            if @provisioned_set[type]
+              @provisioned_set[type].cleanup
+            end
+          end
         end
       end
     end
