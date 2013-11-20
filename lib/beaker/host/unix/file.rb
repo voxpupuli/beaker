6551ca6db58b004ed73da3c51a574ee387200441
Justin Stoller <justin@puppetlabs.com>
2013-11-19 19:29:03 -0800

    Use Beaker::Command when working within hosts

--- lib/beaker/host/unix/file.rb
+++ lib/beaker/host/unix/file.rb
@@ -14,7 +14,7 @@ module Unix::File
   end
 
   def file_exist?(path)
-    result = exec(Command.new("test -e #{path}"), :acceptable_exit_codes => [0, 1])
+    result = exec(Beaker::Command.new("test -e #{path}"), :acceptable_exit_codes => [0, 1])
     result.exit_code == 0
   end
 end
