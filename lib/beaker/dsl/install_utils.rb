0f225cad1c21ea53e1f6e6f4a6df67023d12cf8e
Kurt Wall <kurt.wall@puppetlabs.com>
2014-01-13 14:11:56 -0800

    Update regex to match gitmirror@

--- lib/beaker/dsl/install_utils.rb
+++ lib/beaker/dsl/install_utils.rb
@@ -12,7 +12,7 @@ module Beaker
       SourcePath  = "/opt/puppet-git-repos"
 
       # A regex to know if the uri passed is pointing to a git repo
-      GitURI       = %r{^(git|https?|file)://|^git@|^gitmirror}
+      GitURI       = %r{^(git|https?|file)://|^git@|^gitmirror@}
 
       # Github's ssh signature for cloning via ssh
       GitHubSig   = 'github.com,207.97.227.239 ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ=='
