3bcbfe6d6e339f4df116bdc9ba69315e9e257881
Travis Fields <travis@puppetlabs.com>
2014-06-10 09:06:06 -0700

    Fixed issue with default behavior not picking up type and hosts from rake task

--- lib/beaker/tasks/test.rb
+++ lib/beaker/tasks/test.rb
@@ -1,6 +1,9 @@
 require 'beaker/tasks/rake_task'
 
-Beaker::Tasks::RakeTask.new
+Beaker::Tasks::RakeTask.new do |t,args|
+  t.type  = args[:type]
+  t.hosts = args[:hosts]
+end
 
 desc "Run Beaker PE tests"
 Beaker::Tasks::RakeTask.new("beaker:test:pe",:hosts) do |t,args|
