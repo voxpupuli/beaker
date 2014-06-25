dbc4288cc180e55aa3878ffdeb3ed026f11cfcd0
Eric Shamow <eric.shamow@gmail.com>
2014-06-25 04:28:40 -0700

    Add ability to pass ports as well as roles to EC2 provisioner

--- lib/beaker/hypervisor/ec2_helper.rb
+++ lib/beaker/hypervisor/ec2_helper.rb
@@ -6,23 +6,24 @@ module Beaker
     # @param [Array<String>] roles An array of roles
     # @return [Array<Number>] array of port numbers
     # @api private
-    def self.amiports(roles)
-      ports = [22]
+    def self.amiports(roles, *ports)
+      amiports = [22]
 
       if roles.include? 'database'
-        ports << 8080
-        ports << 8081
+        amiports << 8080
+        amiports << 8081
       end
 
       if roles.include? 'master'
-        ports << 8140
+        amiports << 8140
       end
 
       if roles.include? 'dashboard'
-        ports << 443
+        amiports << 443
       end
 
-      ports
+      amiports << ports
+      amiports.flatten
     end
   end
 end
