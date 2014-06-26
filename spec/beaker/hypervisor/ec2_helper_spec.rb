0042a7f4f4d1e51b86264128bd7e29fa9f99f7f4
Eric Shamow <eric.shamow@gmail.com>
2014-06-25 18:29:26 -0700

    Add test coverage for EC2 port feature

--- spec/beaker/hypervisor/ec2_helper_spec.rb
+++ spec/beaker/hypervisor/ec2_helper_spec.rb
@@ -19,5 +19,13 @@ describe Beaker::EC2Helper do
     it "can set ports for combined master/database/dashboard host" do
       expect(ec2.amiports(["dashboard", "master", "database"])).to be === [22, 8080, 8081, 8140, 443]
     end
+
+    it "can add arbitrary ports passed in an array" do
+      expect(ec2.amiports([],[8081,443])).to be === [22, 8081, 443]
+    end
+
+    it "can handle additional ports when a role is assigned" do
+      expect(ec2.amiports(["master"],[8081,443])).to be === [22, 8140, 8081, 443]
+    end
   end
 end
