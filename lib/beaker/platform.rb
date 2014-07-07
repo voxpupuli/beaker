4ee2980c8dc352ca2a13294031dbd150d16d9930
Patrick Carlisle <patrick@puppetlabs.com>
2014-07-07 11:09:37 -0700

    (MAINT) Whitespace for readability

--- lib/beaker/platform.rb
+++ lib/beaker/platform.rb
@@ -1,10 +1,11 @@
 module Beaker
-  #This class create a Platform object inheriting from String.  It supports all String methods while adding
-  #several platform-specific use cases.
+  # This class create a Platform object inheriting from String.  It supports
+  # all String methods while adding several platform-specific use cases.
   class Platform < String
-    #Supported platforms
+    # Supported platforms
     PLATFORMS = /^(osx|centos|fedora|debian|oracle|redhat|scientific|sles|ubuntu|windows|solaris|aix|el)\-.+\-.+$/
-    #Platform version numbers vs. codenames conversion hash
+
+    # Platform version numbers vs. codenames conversion hash
     PLATFORM_VERSION_CODES =
       { :debian => { "wheezy"  => "7",
                      "squeeze" => "6",
@@ -19,16 +20,20 @@ module Beaker
 
     # A string with the name of the platform.
     attr_reader :variant
+
     # A string with the version number of the platform.
     attr_reader :version
+
     # A string with the codename of the platform+version, nil on platforms
     # without codenames.
     attr_reader :codename
+
     # A string with the cpu architecture of the platform.
     attr_reader :arch
 
-    #Creates the Platform object.  Checks to ensure that the platform String provided meets the platform
-    #formatting rules.  Platforms name must be of the format /^OSFAMILY-VERSION-ARCH.*$/ where OSFAMILY is one of:
+    # Creates the Platform object.  Checks to ensure that the platform String
+    # provided meets the platform formatting rules.  Platforms name must be of
+    # the format /^OSFAMILY-VERSION-ARCH.*$/ where OSFAMILY is one of:
     # * osx
     # * centos
     # * fedora
