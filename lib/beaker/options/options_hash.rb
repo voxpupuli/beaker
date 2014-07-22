fa93b23391863aad01d235b24dffb7795a078844
Alice Nodelman <alice@puppetlabs.com>
2014-07-22 11:53:21 -0700

    (gh-333) Typo in method name ('has' instead of 'hash') in ...
    ... options_hash.rb (line 101)
    
    - yup, fix the typo

--- lib/beaker/options/options_hash.rb
+++ lib/beaker/options/options_hash.rb
@@ -98,7 +98,7 @@ module Beaker
       def rmerge base, hash
         return base unless hash.is_a?(Hash) || hash.is_a?(OptionsHash)
         hash.each do |key, v|
-          if (base[key].is_a?(Hash) || base[key].is_a?(OptionsHash)) && (hash[key].is_a?(Hash) || has[key].is_a?(OptionsHash))
+          if (base[key].is_a?(Hash) || base[key].is_a?(OptionsHash)) && (hash[key].is_a?(Hash) || hash[key].is_a?(OptionsHash))
             rmerge(base[key], hash[key])
           elsif hash[key].is_a?(Hash)
             base[key] = OptionsHash.new.merge(hash[key])
