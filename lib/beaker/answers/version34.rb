215e115fbf02922200561899ccafc0ab41bd1cb5
Nick Lewis <nick@puppetlabs.com>
2014-08-12 14:14:47 -0700

    Revert "(QENG-997) Add PE installer answer for jvm puppet."
    This reverts commit b06b6313de24da82a031b6b04dc95042812b4445.
    
    The installer doesn't actually function with this answer set, so we
    certainly shouldn't be making it the default.

--- lib/beaker/answers/version34.rb
+++ lib/beaker/answers/version34.rb
@@ -4,11 +4,7 @@ module Beaker
   module Answers
     module Version34
       def self.answers(hosts, master_certname, options)
-        master = only_host_with_role(hosts, 'master')
-
         the_answers = Version32.answers(hosts, master_certname, options)
-        the_answers[master.name][:q_jvm_puppetmaster] = 'y'
-
         return the_answers
       end
     end
