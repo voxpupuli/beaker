c713fdb175f30f4027db6d71456a1a1789cddd77
Doug Rosser <doug.rosser@puppetlabs.com>
2014-09-09 13:01:39 -0700

    (QENG-1033) Remove dead rspec code

--- spec/beaker/host_prebuilt_steps_spec.rb
+++ spec/beaker/host_prebuilt_steps_spec.rb
@@ -265,12 +265,6 @@ describe Beaker do
   context "validate_host" do
     subject { dummy_class.new }
 
-    before(:each) do
-      # Must reset additional_pkgs between each test as it hangs around
-      #Beaker::HostPrebuiltSteps.class_variable_set(:@@additional_pkgs, [])
-      Beaker::HostPrebuiltSteps.module_eval(%q{@@additional_pkgs = []})
-    end
-
     it "can validate unix hosts" do
 
       hosts.each do |host|
