469331d15844898756f70bc91874484dd8a64dad
Nikita Drapatiy <vertilon@gmail.com>
2014-09-17 20:15:34 +0300

    Add ability to create more than one network interface when using vagrant

--- lib/beaker/hypervisor/vagrant.rb
+++ lib/beaker/hypervisor/vagrant.rb
@@ -29,7 +29,9 @@ module Beaker
         v_file << "    v.vm.box = '#{host['box']}'\n"
         v_file << "    v.vm.box_url = '#{host['box_url']}'\n" unless host['box_url'].nil?
         v_file << "    v.vm.base_mac = '#{randmac}'\n"
-        v_file << "    v.vm.network :private_network, ip: \"#{host['ip'].to_s}\", :netmask => \"#{host['netmask'] ||= "255.255.0.0"}\"\n"
+        host['ips'].each do |ip|
+          v_file << "    v.vm.network :private_network, ip: \"#{ip.to_s}\", :netmask => \"#{host['netmask'] ||= "255.255.0.0"}\"\n"
+        end
 
         if host['disk_path']
           v_file << "    v.vm.provider :virtualbox do |vb|\n"
