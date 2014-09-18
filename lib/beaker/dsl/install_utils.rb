157572ce73b53e26080dcf07ca67121fff6df276
liamjbennett <lbennett@opentable.com>
2014-09-18 09:28:40 +0100

    switching method to use powershell wrapper

--- lib/beaker/dsl/install_utils.rb
+++ lib/beaker/dsl/install_utils.rb
@@ -653,7 +653,7 @@ module Beaker
         end
         nil
       end
-      
+
       # Configure a host entry on the give host
       # @example: will add a host entry for forge.puppetlabs.com
       #   host_entry(host, { :ip => '23.251.154.122', :name => 'forge.puppetlabs.com' })
@@ -665,7 +665,7 @@ module Beaker
           powershell_pre = "powershell.exe -InputFormat None -NoProfile -NonInteractive -NoLogo -ExecutionPolicy Bypass"
           hosts_file = "C:\\Windows\\System32\\Drivers\\etc\\hosts"
           host_entry = "#{opts['ip']}`t`t#{opts['name']}"
-          on host, "#{powershell_pre} -Command \"\$text = \\\"#{host_entry}\\\"; Add-Content -path '#{hosts_file}' -value \$text\""
+          on host, powershell("\$text = \\\"#{host_entry}\\\"; Add-Content -path '#{hosts_file}' -value \$text")
         else
           hosts_file = "/etc/hosts"
           host_entry = "#{opts['ip']}\t\t#{opts['name']}"
