727f3541467bff99376df4115b133298835663f2
Eric Williamson <eric.williamson@puppetlabs.com>
2014-10-24 16:54:22 -0700

    (maint) open port 5432 on ec2 for postgresql
    Previous to this commit, we never explicitly opened the database port in
    ec2. Either it was somehow implicitly being included/created and that
    broke, or split installs just never worked in ec2.

--- spec/beaker/hypervisor/ec2_helper_spec.rb
+++ spec/beaker/hypervisor/ec2_helper_spec.rb
@@ -5,7 +5,7 @@ describe Beaker::EC2Helper do
   context ".amiports" do
     let(:ec2) { Beaker::EC2Helper }
     it "can set ports for database host" do
-      expect(ec2.amiports(["database"])).to be === [22, 61613, 8139, 8080, 8081]
+      expect(ec2.amiports(["database"])).to be === [22, 61613, 8139, 5432, 8080, 8081]
     end
 
     it "can set ports for master host" do
@@ -17,7 +17,7 @@ describe Beaker::EC2Helper do
     end
 
     it "can set ports for combined master/database/dashboard host" do
-      expect(ec2.amiports(["dashboard", "master", "database"])).to be === [22, 61613, 8139, 8080, 8081, 8140, 443, 4435]
+      expect(ec2.amiports(["dashboard", "master", "database"])).to be === [22, 61613, 8139, 5432, 8080, 8081, 8140, 443, 4435]
     end
   end
 end
