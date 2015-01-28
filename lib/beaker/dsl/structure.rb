1a14e50c1bc4114db8e3ae50b717930946f1700c
Alice Nodelman <alice@puppetlabs.com>
2015-01-28 15:59:13 -0800

    (QENG-1629) Add expect_failure() to Beaker DSL (the code is provided)
    - add expect_failure method
    - add expect_failure spec tests

--- lib/beaker/dsl/structure.rb
+++ lib/beaker/dsl/structure.rb
@@ -1,3 +1,4 @@
+require 'beaker/dsl/assertions'
 module Beaker
   module DSL
     # These are simple structural elements necessary for writing
@@ -21,6 +22,11 @@ module Beaker
     #
     #       step 'Test the things' do
     #         ...tests...
+    #       end
+    #
+    #       step 'Expect this to fail' do
+    #         expect_failure('expected to fail due to PE-1234') do
+    #         assert_equal(400, response.code, 'bad response code from API call')
     #       end
     #     end
     #
@@ -55,8 +61,69 @@ module Beaker
       #     on(master, puppet_resource('file', '/etc/puppet/modules',
       #       'ensure=absent', 'purge=true'))
       #   end
+      # @api dsl
       def teardown &block
         @teardown_procs << block
+      end
+
+      # Wrap an assert that is supposed to fail due to a product bug, an
+      # undelivered feature, or some similar situation.
+      #
+      # This converts failing asserts into passing asserts (so we can continue to
+      # run the test even though there are underlying product bugs), and converts
+      # passing asserts into failing asserts (so we know when the underlying product
+      # bug has been fixed).
+      #
+      # Pass an assert as a code block, and pass an explanatory message as a
+      # parameter. The assert's logic will be inverted (so passes turn into fails
+      # and fails turn into passes).
+      #
+      # @example Typical usage
+      #   expect_failure('expected to fail due to PE-1234') do
+      #     assert_equal(400, response.code, 'bad response code from API call')
+      #   end
+      #
+      # @example Output when a product bug would normally cause the assert to fail
+      #   Warning: An assertion was expected to fail, and did.
+      #   This is probably due to a known product bug, and is probably not a problem.
+      #   Additional info: 'expected to fail due to PE-6995'
+      #   Failed assertion: 'bad response code from API call.
+      #   <400> expected but was <409>.'
+      #
+      # @example Output when the product bug has been fixed
+      #   <RuntimeError: An assertion was expected to fail, but passed.
+      #   This is probably because a product bug was fixed, and "expect_failure()"
+      #   needs to be removed from this assert.
+      #   Additional info: 'expected to fail due to PE-6996'>
+      #
+      # @param [String] explanation A description of why this assert is expected to
+      #                             fail
+      # @param block [Proc] block of code is expected to either raise an
+      #                     {Beaker::Assertions} or else return a value that
+      #                     will be ignored
+      # @raise [RuntimeError] if the code block passed to this method does not raise
+      #                       a {Beaker::Assertions} (i.e., if the assert
+      #                       passes)
+      # @author Chris Cowell-Shah (<tt>ccs@puppetlabs.com</tt>)
+      # @api dsl
+      def expect_failure(explanation, &block)
+        begin
+          yield if block_given?  # code block should contain an assert that you expect to fail
+        rescue Beaker::DSL::Assertions, Minitest::Assertion => failed_assertion
+          # Yay! The assert in the code block failed, as expected.
+          # Swallow the failure so the test passes.
+          logger.notify 'An assertion was expected to fail, and did. ' +
+                          'This is probably due to a known product bug, ' +
+                          'and is probably not a problem. ' +
+                          "Additional info: '#{explanation}' " +
+                          "Failed assertion: '#{failed_assertion}'"
+          return
+        end
+        # Uh-oh! The assert in the code block unexpectedly passed.
+        fail('An assertion was expected to fail, but passed. ' +
+                 'This is probably because a product bug was fixed, and ' +
+                 '"expect_failure()" needs to be removed from this test. ' +
+                 "Additional info: '#{explanation}'")
       end
     end
   end
