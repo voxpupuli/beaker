7d1a25a8d3733408d28c4c07aa9431524319af43
Djuri Baars <dsbaars@gmail.com>
2015-01-31 22:10:27 +0100

    Added Vagrant Parallels Hypervisor

---
+++ lib/beaker/hypervisor/vagrant_parallels.rb
@@ -0,0 +1,18 @@
+require 'beaker/hypervisor/vagrant'
+
+class Beaker::VagrantParallels < Beaker::Vagrant
+    def provision(provider = 'parallels')
+        super
+    end
+
+    def self.provider_vfile_section(host, options)
+        provider_section  = ""
+        provider_section << "    v.vm.provider :parallels do |prl|\n"
+        provider_section << "      prl.optimize_power_consumption = false\n"
+        provider_section << "      prl.memory = ', '#{options['vagrant_memsize'] ||= '1024'}']\n"
+        provider_section << "      prl.update_guest_tools = false\n" if options[:prl_update_guest_tools] == 'disable'
+        provider_section << "    end\n"
+
+        provider_section
+    end
+end
