0b7afd494fb2adf10e04744894d6fb50365550f3
kevpl <kevin.imber@puppetlabs.com>
2015-03-05 13:41:49 -0800

    (QENG-1946) removed paths from AIO host defaults in windows & unix

--- lib/beaker/dsl/helpers.rb
+++ lib/beaker/dsl/helpers.rb
@@ -1405,7 +1405,7 @@ module Beaker
           hiera_config=Hash.new
           hiera_config[:backends] = 'yaml'
           hiera_config[:yaml] = {}
-          hiera_config[:yaml][:datadir] = host[:hieradatadir]
+          hiera_config[:yaml][:datadir] = hiera_datadir(host)
           hiera_config[:hierarchy] = hierarchy
           hiera_config[:logger] = 'console'
           create_remote_file host, host.puppet['hiera_config'], hiera_config.to_yaml
@@ -1424,13 +1424,23 @@ module Beaker
       #                           or a role (String or Symbol) that identifies one or more hosts.
       # @param[String]            Directory containing the hiera data files.
       def copy_hiera_data_to(host, source)
-        scp_to host, File.expand_path(source), host[:hieradatadir]
+        scp_to host, File.expand_path(source), hiera_datadir(host)
       end
 
       # Copy hiera data files to the default host
       # @see #copy_hiera_data_to
       def copy_hiera_data(source)
         copy_hiera_data_to(default, source)
+      end
+
+      # Get file path to the hieradatadir for a given host.
+      # Handles whether or not a host is AIO-based & backwards compatibility
+      #
+      # @param[Host] host Host you want to use the hieradatadir from
+      #
+      # @return [String] Path to the hiera data directory
+      def hiera_datadir(host)
+        host[:type] =~ /aio/ ? File.join(host.puppet['codedir'], 'hieradata') : host[:hieradatadir]
       end
 
     end
