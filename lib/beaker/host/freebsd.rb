e288f713284cbbed9b54bf2a17a7e088c16e9985
Peter Souter <peter.souter@puppetlabs.com>
2015-03-30 10:33:48 +0100

    (BKR-113) Add FreeBSD host class
    * Also add to supported platforms
    * Main change is location of Puppet data under `/usr/local/etc` rather than `/etc/`

---
+++ lib/beaker/host/freebsd.rb
@@ -0,0 +1,31 @@
+[ 'host', 'command_factory' ].each do |lib|
+  require "beaker/#{lib}"
+end
+
+module FreeBSD
+  class Host < Unix::Host
+
+    def self.foss_defaults
+      h = Beaker::Options::OptionsHash.new
+      h.merge({
+        'user'              => 'root',
+        'group'             => 'puppet',
+        'puppetserver-confdir' => '/etc/puppetserver/conf.d',
+        'puppetservice'     => 'puppetmaster',
+        'puppetpath'        => '/usr/local/etc/puppet/modules',
+        'puppetvardir'      => '/var/lib/puppet',
+        'puppetbin'         => '/usr/bin/puppet',
+        'puppetbindir'      => '/usr/bin',
+        'hieralibdir'       => '/opt/puppet-git-repos/hiera/lib',
+        'hierapuppetlibdir' => '/opt/puppet-git-repos/hiera-puppet/lib',
+        'hierabindir'       => '/opt/puppet-git-repos/hiera/bin',
+        'hieradatadir'      => '/usr/local/etc/puppet/modules/hieradata',
+        'hieraconf'         => '/usr/local/etc/puppet/modules/hiera.yaml',
+        'distmoduledir'     => '/usr/local/etc/puppet/modules',
+        'sitemoduledir'     => '/usr/share/puppet/modules',
+        'pathseparator'     => ':',
+        })
+    end
+  end
+
+end
