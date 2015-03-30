39038ad3f202a5f6e8e3c02d0bd1d18ce816fb27
Peter Souter <peter.souter@puppetlabs.com>
2015-03-30 10:52:08 +0100

    (BKR-113) Host Prebuilt commands for FreeBSD
    Since FreeBSD echo is different from Linux-y echo, it's much easier to just use printf and escape the strings using gsub.

---
+++ lib/beaker/host/freebsd/exec.rb
@@ -0,0 +1,10 @@
+module FreeBSD::Exec
+  include Beaker::CommandFactory
+
+  def echo_to_file(str, filename)
+    # FreeBSD gets weird about special characters, we have to go a little OTT here
+    escaped_str = str.gsub(/\t/,'\\t').gsub(/\n/,'\\n')
+
+    exec(Beaker::Command.new("printf #{escaped_str} > #{filename}"))
+  end
+end
