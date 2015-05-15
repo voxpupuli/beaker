0a6a2a96a37dfe3001717b449b1f7acea93588d2
kevpl <kevin.imber@puppetlabs.com>
2015-05-15 15:05:15 -0700

    (BKR-232) --no-provision now implies --no-configure & --no-validate

--- spec/beaker/options/command_line_parser_spec.rb
+++ spec/beaker/options/command_line_parser_spec.rb
@@ -6,12 +6,18 @@ module Beaker
 
       let(:parser) {Beaker::Options::CommandLineParser.new}
       let(:test_opts) {["-h", "vcloud.cfg", "--debug", "--tests", "test.rb", "--help"]}
-      let(:full_opts_in)  {["--hosts", "host.cfg",           "--options", "opts_file", "--type", "pe", "--helper", "path_to_helper", "--load-path", "load_path", "--tests", "test1.rb,test2.rb,test3.rb", "--pre-suite", "pre_suite.rb", "--post-suite", "post_suite.rb", "--no-provision", "--preserve-hosts", "always",   "--root-keys", "--keyfile", "../.ssh/id_rsa", "--install", "gitrepopath",     "-m", "module",         "-q",    "--dry-run",       "--no-ntp",    "--repo-proxy",    "--add-el-extras", "--config", "anotherfile.cfg", "--fail-mode", "fast",  "--no-color",    "--version", "--log-level", "info", "--package-proxy", "http://192.168.100.1:3128",    "--collect-perf-data",    "--parse-only",    "--validate", "--timeout", "40", "--log-prefix", "pants"]}
-      let(:full_opts_out) {{:hosts_file=>"anotherfile.cfg",:options_file=>"opts_file",  :type => "pe",  :helper => "path_to_helper",  :load_path => "load_path",  :tests => "test1.rb,test2.rb,test3.rb",  :pre_suite => "pre_suite.rb",  :post_suite => "post_suite.rb", :provision=>false, :preserve_hosts => "always", :root_keys=>true, :keyfile => "../.ssh/id_rsa",  :install => "gitrepopath", :modules=>"module", :quiet=>true, :dry_run=>true, :timesync=>false, :repo_proxy=>true, :add_el_extras=>true,                                 :fail_mode => "fast", :color=>false, :version=>true,  :log_level => "info",  :package_proxy => "http://192.168.100.1:3128", :collect_perf_data=>true, :parse_only=>true, :validate=>true,  :timeout => "40",  :log_prefix => "pants"}}
+      let(:full_opts_in)  {["--hosts", "host.cfg",           "--options", "opts_file", "--type", "pe", "--helper", "path_to_helper", "--load-path", "load_path", "--tests", "test1.rb,test2.rb,test3.rb", "--pre-suite", "pre_suite.rb", "--post-suite", "post_suite.rb", "--no-provision", "--preserve-hosts", "always",   "--root-keys", "--keyfile", "../.ssh/id_rsa", "--install", "gitrepopath",     "-m", "module",         "-q",    "--dry-run",       "--no-ntp",    "--repo-proxy",    "--add-el-extras", "--config", "anotherfile.cfg", "--fail-mode", "fast",  "--no-color",    "--version", "--log-level", "info", "--package-proxy", "http://192.168.100.1:3128",    "--collect-perf-data",    "--parse-only",    "--validate", "--timeout", "40", "--log-prefix", "pants",      "--configure"]}
+      let(:full_opts_out) {{:hosts_file=>"anotherfile.cfg",:options_file=>"opts_file",  :type => "pe",  :helper => "path_to_helper",  :load_path => "load_path",  :tests => "test1.rb,test2.rb,test3.rb",  :pre_suite => "pre_suite.rb",  :post_suite => "post_suite.rb", :provision=>false, :preserve_hosts => "always", :root_keys=>true, :keyfile => "../.ssh/id_rsa",  :install => "gitrepopath", :modules=>"module", :quiet=>true, :dry_run=>true, :timesync=>false, :repo_proxy=>true, :add_el_extras=>true,                                 :fail_mode => "fast", :color=>false, :version=>true,  :log_level => "info",  :package_proxy => "http://192.168.100.1:3128", :collect_perf_data=>true, :parse_only=>true, :validate=>true,  :timeout => "40",  :log_prefix => "pants", :configure => true}}
       let(:validate_true)  {["--validate"]}
       let(:validate_false) {["--no-validate"]}
       let(:configure_true)  {['--configure']}
       let(:configure_false) {['--no-configure']}
+      let(:provision_false) {['--no-provision']}
+      let(:provision_other) {{:provision => false, :configure => false, :validate => false}}
+      let(:provision_both_in)   {['--no-provision', '--configure', '--validate']}
+      let(:provision_both_out)  {{:provision => false, :configure => true, :validate => true}}
+      let(:provision_half_in)   {['--no-provision', '--configure']}
+      let(:provision_half_out)  {{:provision => false, :configure => true, :validate => false}}
 
 
       it "can correctly read command line input" do
@@ -34,6 +40,20 @@ module Beaker
 
       it "can produce a usage description" do
         expect{parser.usage}.to_not raise_error 
+      end
+
+      context '--no-provision flag effects other options' do
+        it 'sets --no-validate/configure when --no-provision is set' do
+          expect(parser.parse(provision_false)).to be === provision_other
+        end
+
+        it 'can still have --validate & --configure set correctly when --no-provision is set' do
+          expect(parser.parse(provision_both_in)).to be === provision_both_out
+        end
+
+        it 'can override just one of the two flags when --no-provision is set' do
+          expect(parser.parse(provision_half_in)).to be === provision_half_out
+        end
       end
 
     end
