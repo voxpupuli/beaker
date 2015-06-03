986315756c58e25f65f6601ad40f8e6ccd13616c
Rick Bradley <rick@rickbradley.com>
2015-06-03 15:33:26 -0500

    (MAINT) clean up whitespace in spec file

--- spec/beaker/hypervisor/vmpooler_spec.rb
+++ spec/beaker/hypervisor/vmpooler_spec.rb
@@ -7,13 +7,14 @@ module Beaker
       vms = make_hosts()
       MockVsphereHelper.set_config( fog_file_contents )
       MockVsphereHelper.set_vms( vms )
-     stub_const( "VsphereHelper", MockVsphereHelper )
-     stub_const( "Net", MockNet )
+      stub_const( "VsphereHelper", MockVsphereHelper )
+      stub_const( "Net", MockNet )
       allow( JSON ).to receive( :parse ) do |arg|
         arg
       end
       allow( Socket ).to receive( :getaddrinfo ).and_return( true )
-      allow_any_instance_of( Beaker::Vmpooler ).to receive(:load_credentials).and_return(fog_file_contents)
+      allow_any_instance_of( Beaker::Vmpooler ).to \
+        receive(:load_credentials).and_return(fog_file_contents)
     end
 
     describe '#get_template_url' do
@@ -23,7 +24,7 @@ module Beaker
         uri = vmpooler.get_template_url("http://pooling.com", "template")
         expect( uri ).to be === "http://pooling.com/vm/template"
       end
-      
+
       it 'adds a missing scheme to a given URL' do
         vmpooler = Beaker::Vmpooler.new( make_hosts, make_opts )
         uri = vmpooler.get_template_url("pooling.com", "template")
@@ -39,13 +40,11 @@ module Beaker
         vmpooler = Beaker::Vmpooler.new( make_hosts, make_opts )
         expect{ vmpooler.get_template_url("pooling.com", "t!e&m*p(l\\a/t e")}.to raise_error ArgumentError
       end
-      
     end
 
     describe "#provision" do
 
-      it 'provisions hosts from the pool' do 
-
+      it 'provisions hosts from the pool' do
         vmpooler = Beaker::Vmpooler.new( make_hosts, make_opts )
         allow( vmpooler ).to receive( :require ).and_return( true )
         allow( vmpooler ).to receive( :sleep ).and_return( true )
@@ -55,9 +54,7 @@ module Beaker
         hosts.each do | host |
           expect( host['vmhostname'] ).to be === 'pool'
         end
-        
       end
-
     end
 
     describe "#cleanup" do
