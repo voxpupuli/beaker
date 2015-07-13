2a3d9dc5f4bc6e6cafaa7fcadd8303db52d486f0
Alice Nodelman <alice@puppetlabs.com>
2015-07-13 14:52:43 -0700

    (BKR-340) separate answer file generation into a library
    - remove answer file generation from beaker
    - add dependencies to new beaker-answers gem and stringify-hash

--- spec/beaker/options/presets_spec.rb
+++ spec/beaker/options/presets_spec.rb
@@ -10,6 +10,14 @@ module Beaker
         expect(presets.env_vars).to be_instance_of(Beaker::Options::OptionsHash)
       end
 
+      it "pulls in env vars of the form ':q_*' and adds them to the :answers of the OptionsHash" do
+         ENV['q_puppet_cloud_install'] = 'n'
+         env = presets.env_vars
+         expect(env[:answers][:q_puppet_cloud_install]).to be === 'n'
+         expect(env[:answers]['q_puppet_cloud_install']).to be === 'n'
+         ENV.delete('q_puppet_cloud_install')
+      end
+
       it "removes all empty/nil entries in env_vars" do
         expect(presets.env_vars.has_value?(nil)).to be === false
         expect(presets.env_vars.has_value?({})).to be === false
