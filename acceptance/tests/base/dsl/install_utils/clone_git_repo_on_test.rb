7be268cb1eaf1bfbedd21987bd73e47e6766ee57
Rick Bradley <rick@rickbradley.com>
2015-09-11 14:20:49 -0500

    (BKR-358) get all builds green
    I'm not 100% thrilled with this approach (use git for clones on cygwin, https
    everywhere else), but getting the build green is more important now, and we
    can refactor or change approaches later (which seems like a matter of getting
    the appropriate credentials in the appropriate places).
    
    The root problem on Windows is that when attempting to clone a non-existent
    repo on GitHub, we are prompted for username/password for https auth.

--- acceptance/tests/base/dsl/install_utils/clone_git_repo_on_test.rb
+++ acceptance/tests/base/dsl/install_utils/clone_git_repo_on_test.rb
@@ -38,19 +38,24 @@ test_name 'Clone from git' do
   ENV['HIERA_FORK']='puppetlabs'
   ENV['FORK']='fail'
 
+  # return a git URL to the named repository, for the supplied host.
+  def git_url_for(host, repo_name)
+    host.is_cygwin? ? build_git_url(repo_name, nil, nil, 'git') : build_git_url(repo_name)
+  end
+
   # implicitly tests build_giturl() and lookup_in_env()
   hosts.each do |host|
     on host, "echo #{GitHubSig} >> $HOME/.ssh/known_hosts"
     testdir = tmpdir_on(host, File.basename(__FILE__))
 
     step 'should find fork name from the correct environment variable' do
-      results = clone_git_repo_on(host, "#{testdir}", extract_repo_info_from(build_git_url('puppet', nil, nil, 'git')))
+      results = clone_git_repo_on(host, "#{testdir}", extract_repo_info_from(git_url_for(host, 'puppet')))
       assert_match( %r{github\.com[:/]fail}, result.cmd, 'Did not find correct fork name')
       assert_equal( 1, result.exit_code, 'Did not produce error exit_code of 1')
     end
 
     step 'should clone hiera from correct fork' do
-      results = clone_git_repo_on(host, "#{testdir}", extract_repo_info_from(build_git_url('hiera', nil, nil, 'git')))
+      results = clone_git_repo_on(host, "#{testdir}", extract_repo_info_from(git_url_for(host, 'hiera')))
       assert_match( %r{From.*github\.com[:/]puppetlabs/hiera}, result.output, 'Did not find clone')
     end
   end
