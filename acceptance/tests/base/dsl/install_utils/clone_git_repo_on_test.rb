d1c429f45e99de8603f06dd4a12d28ddc9f04d94
Rick Bradley <rick@rickbradley.com>
2015-09-11 15:43:58 -0500

    (BKR-358) streamline clone_git_repo_on acceptance test
    Prior to this we were still seeing issues with getting this acceptance test
    passing on all platforms.  The core issue on windows platforms is that when
    attempting to clone a non-existent repository from github, credentials are
    asked for (https) which appear not to be set up; and for the git protocol,
    the appropriate SSH credentials also appear not to be in place.
    
    Taking a closer look at the test, it was somewhat confused: in addition to
    testing git clones, it was also trying to test the vagaries of `build_git_url`
    and its environment settings (via `lookup_in_env`).  There are already spec
    tests for some of this behavior, so testing it here is mostly a waste of
    resources.
    
    This commit bolsters those specs to test more of the ENV setting functionality
    behind `build_git_url` (also aliased as `build_giturl`), and trims this test
    down to doing a successful git clone, in hopes this will make the test happy
    on all platforms.

--- acceptance/tests/base/dsl/install_utils/clone_git_repo_on_test.rb
+++ acceptance/tests/base/dsl/install_utils/clone_git_repo_on_test.rb
@@ -34,28 +34,13 @@ test_name 'Clone from git' do
 
   install_packages_on(hosts, PACKAGES, :check_if_exists => true)
 
-  # build_giturl implicitly looks these up
-  ENV['HIERA_FORK']='puppetlabs'
-  ENV['FORK']='fail'
-
-  # return a git URL to the named repository, for the supplied host.
-  def git_url_for(host, repo_name)
-    host.is_cygwin? ? build_git_url(repo_name, nil, nil, 'git') : build_git_url(repo_name)
-  end
-
   # implicitly tests build_giturl() and lookup_in_env()
   hosts.each do |host|
     on host, "echo #{GitHubSig} >> $HOME/.ssh/known_hosts"
     testdir = tmpdir_on(host, File.basename(__FILE__))
 
-    step 'should find fork name from the correct environment variable' do
-      results = clone_git_repo_on(host, "#{testdir}", extract_repo_info_from(git_url_for(host, 'puppet')))
-      assert_match( %r{github\.com[:/]fail}, result.cmd, 'Did not find correct fork name')
-      assert_equal( 1, result.exit_code, 'Did not produce error exit_code of 1')
-    end
-
-    step 'should clone hiera from correct fork' do
-      results = clone_git_repo_on(host, "#{testdir}", extract_repo_info_from(git_url_for(host, 'hiera')))
+    step 'should be able to successfully clone a git repo' do
+      results = clone_git_repo_on(host, "#{testdir}", extract_repo_info_from(build_git_url('hiera')))
       assert_match( %r{From.*github\.com[:/]puppetlabs/hiera}, result.output, 'Did not find clone')
     end
   end
