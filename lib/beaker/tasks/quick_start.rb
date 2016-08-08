1539d33f848671885c803d54eaaeae4721d8e7f0
sarah-thompson <sarah.thompson@puppetlabs.com>
2016-08-08 15:57:40 +0100

    Adding tutorial doc.

--- lib/beaker/tasks/quick_start.rb
+++ lib/beaker/tasks/quick_start.rb
@@ -1,29 +1,33 @@
 require 'beaker-hostgenerator'
 
-VAGRANT  = BeakerHostGenerator::CLI.new(['ubuntu1404-64default.mdcal', '--hypervisor=vagrant',
-                                         '--global-config={box_url=https://vagrantcloud.com/puppetlabs/boxes/ubuntu-14.04-64-nocm,box=puppetlabs/ubuntu-14.04-64-nocm'])
-VMPOOLER = BeakerHostGenerator::CLI.new(['redhat7-64default.mdcal-redhat7-64af'])
+VAGRANT  = ['ubuntu1404-64default.mdcal-ubuntu1404-64af', '--hypervisor=vagrant',
+            '--global-config={box_url=https://vagrantcloud.com/puppetlabs/boxes/ubuntu-14.04-64-nocm,box=puppetlabs/ubuntu-14.04-64-nocm}']
+
+VMPOOLER = ['redhat7-64default.mdcal-redhat7-64af']
 
 namespace :beaker_quickstart do
 
   desc 'Generate Default Beaker Host Config File, valid options are: vmpooler or vagrant.'
-  task :gen_hosts, [:provisioner] do |t, args|
-    hosts_file = "acceptance/config/default__#{args[:provisioner]}_hosts.yaml"
-    if args[:provisioner] == 'vagrant'
+  task :gen_hosts, [:hypervisor] do |t, args|
+    hosts_file = "acceptance/config/default_#{args[:hypervisor]}_hosts.yaml"
+    if args[:hypervisor] == 'vagrant'
       cli = VAGRANT
-    elsif args[:provisioner] == 'vmpooler'
+    elsif args[:hypervisor] == 'vmpooler'
       cli = VMPOOLER
     else
-      puts "No provisioner provided, defaulting to vmpooler."
-      cli = VMPOOLER
+      puts "No hypervisor provided, defaulting to vagrant."
+      hosts_file = "acceptance/config/default_vagrant_hosts.yaml"
+      cli = VAGRANT
     end
     FileUtils.mkdir_p('acceptance/config') # -p ignores when dir already exists
-    if File.exist?(hosts_file) then
+    puts "About to run - beaker-hostgenerator #{cli.to_s.delete!('[]"')}"
+    if !File.exist?(hosts_file) then
+      puts "Writing default host config to file - #{hosts_file}"
       File.open(hosts_file, 'w') do |fh|
-        fh.print(cli.execute)
+        fh.print(BeakerHostGenerator::CLI.new(cli).execute)
       end
     else
-      puts "Host Config File: #{hosts_file} already exists so not overwriting"
+      puts "Not overwriting Host Config File: #{hosts_file} - it already exists."
     end
   end
 
@@ -32,42 +36,47 @@ namespace :beaker_quickstart do
   task :gen_pre_suite do
     pre_suite_file = "acceptance/setup/default_pre_suite.rb"
     FileUtils.mkdir_p('acceptance/setup') # -p ignores when dir already exists
-    if File.exist?(pre_suite_file) then
+    if !File.exist?(pre_suite_file) then
+      puts "Writing default pre_suite to file - #{pre_suite_file}"
       File.open(pre_suite_file, 'w') do |fh|
         fh.print('install_puppet')
       end
     else
-      puts "Pre Suite File: #{pre_suite_file} already exists so not overwriting"
+      puts "Not overwriting Pre Suite File: #{pre_suite_file} - it already exists."
     end
   end
 
   desc 'Generate Default Smoke Test'
   task :gen_smoke_test do
-    smoke_test_file = "acceptance/setup/default_pre_suite.rb"
+    smoke_test_file = "acceptance/setup/default_smoke_test.rb"
     FileUtils.mkdir_p('acceptance/tests') # -p ignores when dir already exists
-    if File.exist?(smoke_test_file) then
+    if !File.exist?(smoke_test_file) then
+      puts "Writing default smoke test to file - #{smoke_test_file}"
       File.open("acceptance/tests/default_smoke_test.rb", 'w') do |fh|
-        fh.print("test_name 'puppet install smoketest'
-step 'puppet install smoketest: verify \\'puppet help\\' can be successfully called on
-all hosts'
+        fh.print("test_name 'puppet install smoketest' do
+  step 'puppet install smoketest: verify \\'puppet help\\' can be successfully called on
+  all hosts' do
     hosts.each do |host|
       on host, puppet('help')
-    end")
+    end
+  end
+end")
       end
     else
-      puts "Smoke Test File: #{smoke_test_file} already exists so not overwriting"
+      puts "Not overwriting Smoke Test File: #{smoke_test_file} - it already exists."
     end
   end
 
   desc 'Run Default Smoke Test, after generating default host config and test files, valid options are: vmpooler or vagrant.'
-  task :run_test, [:provisioner] => ["beaker_quickstart:gen_hosts", 'beaker_quickstart:gen_pre_suite',
+  task :run_test, [:hypervisor] => ["beaker_quickstart:gen_hosts", 'beaker_quickstart:gen_pre_suite',
                                      'beaker_quickstart:gen_smoke_test'] do
   |t, args|
-    provisioner             = args[:provisioner] ||='vmpooler'
+    hypervisor             = args[:hypervisor] ||='vagrant'
     system_args             = Hash.new
-    system_args[:hosts]     = "acceptance/config/default_#{provisioner}_hosts.yaml"
+    system_args[:hosts]     = "acceptance/config/default_#{hypervisor}_hosts.yaml"
     system_args[:pre_suite] = 'acceptance/setup/default_pre_suite.rb'
     system_args[:tests]     = 'acceptance/tests/default_smoke_test.rb'
+    puts "About to run - #{beaker_command(system_args)}"
     system(beaker_command(system_args))
   end
 
@@ -75,7 +84,6 @@ end
 
 
 def beaker_command(system_args)
-  binding.pry
   cmd_parts = []
   cmd_parts << "beaker"
   cmd_parts << "--hosts #{system_args[:hosts]}"
