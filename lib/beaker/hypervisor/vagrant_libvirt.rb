40ecb249329e3ce4bcadd27a92d26582e1545f01
sathlan <chem@sathlan.org>
2016-11-07 17:18:31 +0100

    (BKR-915) Add any option to libvirt provider (#1222)
    * Add cpu option for libvirt provider.
    
    * (BKR 915) Add any option to libvirt provider.
    
    * Adding spec tests for libvirt cpu and any options.
    
    * Adding documentation for the vagrant-libvirt plugin.

--- lib/beaker/hypervisor/vagrant_libvirt.rb
+++ lib/beaker/hypervisor/vagrant_libvirt.rb
@@ -2,6 +2,7 @@ require 'beaker/hypervisor/vagrant'
 
 class Beaker::VagrantLibvirt < Beaker::Vagrant
   @memory = nil
+  @cpu    = nil
 
   class << self
     attr_reader :memory
@@ -13,8 +14,22 @@ class Beaker::VagrantLibvirt < Beaker::Vagrant
 
   def self.provider_vfile_section(host, options)
     "    v.vm.provider :libvirt do |node|\n" +
+      "      node.cpus = #{cpu(host, options)}\n" +
       "      node.memory = #{memory(host, options)}\n" +
+      build_options_str(options) +
       "    end\n"
+  end
+
+  def self.cpu(host, options)
+    return @cpu unless @cpu.nil?
+    @cpu = case
+    when host['vagrant_cpus']
+      host['vagrant_cpus']
+    when options['vagrant_cpus']
+      options['vagrant_cpus']
+    else
+      '1'
+    end
   end
 
   def self.memory(host, options)
@@ -27,5 +42,17 @@ class Beaker::VagrantLibvirt < Beaker::Vagrant
     else
       '512'
     end
+  end
+
+  def self.build_options_str(options)
+    other_options_str = ''
+    if options['libvirt']
+      other_options = []
+      options['libvirt'].each do |k, v|
+        other_options << "      node.#{k} = '#{v}'"
+      end
+      other_options_str = other_options.join("\n")
+    end
+    "#{other_options_str}\n"
   end
 end
