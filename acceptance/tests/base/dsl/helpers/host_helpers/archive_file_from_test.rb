79a72e61d54a0d043e70cc1280d6302d7b1e2d15
Spencer McElmurry <mcelmurs@gmail.com>
2016-11-10 09:50:06 -0800

    (QENG-4170) Compress files in archive_file_from method (#1272)
    * (QENG-4170) Compress files in archive_file_from method
    
    Adding tarball compression to archive_file_from. Saves space
    when saving artifacts to jenkins masters.
    
    * Adding additional documentation and tests/checks for archive_file_from method
    
    * Add raise exception to archive_file_from to check for non-existant files
    Altered acceptance tests to follow suit.

--- acceptance/tests/base/dsl/helpers/host_helpers/archive_file_from_test.rb
+++ acceptance/tests/base/dsl/helpers/host_helpers/archive_file_from_test.rb
@@ -19,17 +19,29 @@ test_name "dsl::helpers::host_helpers #archive_file_from" do
     assert_equal(true, Dir.exists?('archive/sut-files'))
   end
 
-  step "file is copied to local <archiveroot>/<host name> directory" do
+  step "fails archive_file_from when from_path is non-existant" do
+    filepath = "foo-filepath-should-not-exist"
+    assert_raises IOError do
+      archive_file_from(default, filepath)
+    end
+  end
+
+  step "archive is copied to local <archiveroot>/<hostname>/<filepath> directory" do
     # Create a remote file to archive
     filepath = default.tmpfile('archive-file-test')
     create_remote_file(default, filepath, 'number of the beast')
+    assert_equal(false, Dir.exists?(filepath))
 
     # Test that the file is copied locally to <archiveroot>/<hostname>/<filepath>
     Dir.mktmpdir do |tmpdir|
-      archive_file_from(default, filepath, {}, tmpdir)
-      expected_path = File.join(tmpdir, default, filepath)
-      assert(File.exists?(expected_path))
-      assert_equal('number of the beast', File.read(expected_path).strip)
+      tar_path = File.join(tmpdir, default, filepath + '.tgz')
+      archive_file_from(default, filepath, {}, tmpdir, tar_path)
+      assert(File.exists?(tar_path))
+      expected_path = File.join(tmpdir, default)
+
+      tgz = Zlib::GzipReader.new(File.open(tar_path, 'rb'))
+      Archive::Tar::Minitar.unpack(tgz, expected_path)
+      assert_equal('number of the beast', File.read(expected_path + '/' + filepath).strip)
     end
   end
 end
