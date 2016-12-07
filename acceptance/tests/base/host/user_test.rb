5b19a7835450033497024d10a5a73d66f748d5db
Iristyle <Iristyle@github>
2016-12-06 18:24:39 -0800

    (BKR-1008) Fix Windows user_get error detection
     - The existing mechanism is tightly coupled to English / ASCII output
       and should instead use exit codes.
    
       Fix both the PowerShell and CMD hosts.
    
       Add a new acceptance test so that this can be validated on a
       non-English Windows host (for instance Japanse Windows 2012R2
       template win-2012r2-ja-x86_64)

---
+++ acceptance/tests/base/host/user_test.rb
@@ -0,0 +1,18 @@
+test_name 'User Test' do
+  step "#user_get: has an Administrator user on Windows" do
+    hosts.select { |h| h['platform'] =~ /windows/ }.each do |host|
+      host.user_get('Administrator') do |result|
+        refute_match(result.stdout, 'NET HELPMSG', 'Output indicates Administrator not found')
+      end
+    end
+  end
+
+  step "#user_get: should not have CaptainCaveman user on Windows" do
+    hosts.select { |h| h['platform'] =~ /windows/ }.each do |host|
+      assert_raises Beaker::Host::CommandFailure do
+        host.user_get('CaptainCaveman') { |result| }
+      end
+    end
+  end
+
+end
