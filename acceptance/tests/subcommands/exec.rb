640f6ad95b51e8ea186812c3fe95677b772b7776
Tony Vu <tony.vu@puppet.com>
2017-03-14 17:26:46 -0700

    (BKR-1004) beaker exec implementation
    These changes introduce new subcommand functionality for beaker; the
    exec subcommand takes a file, directory, or beaker suite name and
    launches beaker to test that resource specifically. If no resource is
    specified, it reads in default configuration that should already be set
    up by previous subcommands.

---
+++ acceptance/tests/subcommands/exec.rb
@@ -0,0 +1,37 @@
+test_name 'use the exec subcommand' do
+
+  def delete_root_folder_contents
+    on default, 'rm -rf /root/* /root/.beaker'
+  end
+
+  step 'ensure the workspace is clean' do
+    delete_root_folder_contents
+  end
+
+  step 'run init and provision to set up the system' do
+    on default, 'beaker init --hosts centos6-64; beaker provision'
+    subcommand_state = on(default, 'cat .beaker/.subcommand_state.yaml').stdout
+    subcommand_state = YAML.parse(subcommand_state).to_ruby
+    assert_equal(true, subcommand_state['provisioned'])
+  end
+
+  step 'create a test dir and populate it with tests' do
+    on default, 'mkdir -p testing_dir'
+  end
+
+  step 'create remote test file' do
+    testfile = <<-TESTFILE
+    on(agents, 'echo hello world')
+    TESTFILE
+    create_remote_file(default, '/root/testing_dir/testfile1.rb', testfile)
+  end
+
+  step 'specify that remote file with beaker exec' do
+    result = on(default, 'beaker exec testing_dir/testfile1.rb --log-level verbose')
+    assert_match(/hello world/, result.stdout)
+  end
+end
+
+
+
+
--- spec/beaker/subcommand_spec.rb
+++ spec/beaker/subcommand_spec.rb
@@ -53,20 +53,67 @@ module Beaker
       let ( :cli ) { subcommand.instance_variable_get(:@cli) }
       let( :yaml_store_mock ) { double('yaml_store_mock') }
       let ( :host_hash ) { {'mynode.net' => {:name => 'mynode', :platform => Beaker::Platform.new('centos-6-x86_64')}}}
+      let ( :cleaned_hosts ) {double()}
+      let ( :yielded_host_hash ) {double()}
+      let ( :yielded_host_name) {double()}
       it 'provisions the host and saves the host info' do
         expect(YAML::Store).to receive(:new).with(SubcommandUtil::SUBCOMMAND_STATE).and_return(yaml_store_mock)
         allow(yaml_store_mock).to receive(:[]).and_return(false)
         expect(cli).to receive(:parse_options)
         expect(cli).to receive(:provision)
         expect(cli).to receive(:combined_instance_and_options_hosts).and_return(host_hash)
-        expect(SubcommandUtil).to receive(:sanitize_options_for_save).and_return('cleaned hosts')
+        expect(SubcommandUtil).to receive(:sanitize_options_for_save).and_return(cleaned_hosts)
+        expect(cleaned_hosts).to receive(:each).and_yield(yielded_host_name, yielded_host_hash)
+        expect(yielded_host_hash).to receive(:[]=).with('provision', false)
         expect(YAML::Store).to receive(:new).with(SubcommandUtil::SUBCOMMAND_OPTIONS).and_return(yaml_store_mock)
 
         expect(yaml_store_mock).to receive(:transaction).and_yield.exactly(3).times
-        expect(yaml_store_mock).to receive(:[]=).with('HOSTS', 'cleaned hosts')
+        expect(yaml_store_mock).to receive(:[]=).with('HOSTS', cleaned_hosts)
 
         expect(yaml_store_mock).to receive(:[]=).with('provisioned', true)
         subcommand.provision
+      end
+    end
+
+
+    context 'exec' do
+      it 'calls execute! when no resource is given' do
+        expect_any_instance_of(Pathname).to_not receive(:directory?)
+        expect_any_instance_of(Pathname).to_not receive(:exist?)
+        expect_any_instance_of(Beaker::CLI).to receive(:parse_options).once
+        expect_any_instance_of(Beaker::CLI).to receive(:initialize_network_manager).once
+        expect_any_instance_of(Beaker::CLI).to receive(:execute!).once
+        expect{subcommand.exec}.to_not raise_error
+      end
+
+      it 'checks to to see if the resource is a file_resource' do
+
+        expect_any_instance_of(Pathname).to receive(:exist?).and_return(true)
+        expect_any_instance_of(Pathname).to receive(:directory?).and_return(false)
+        expect_any_instance_of(Pathname).to receive(:expand_path).once
+        expect_any_instance_of(Beaker::CLI).to receive(:execute!).once
+        expect{subcommand.exec('resource')}.to_not raise_error
+      end
+
+      it 'checks to see if the resource is a directory' do
+        expect_any_instance_of(Pathname).to receive(:exist?).and_return(true)
+        expect_any_instance_of(Pathname).to receive(:directory?).and_return(true)
+        expect(Dir).to receive(:glob)
+        expect_any_instance_of(Pathname).to receive(:expand_path).once
+        expect_any_instance_of(Beaker::CLI).to receive(:execute!).once
+        expect{subcommand.exec('resource')}.to_not raise_error
+      end
+
+      it 'allows a hard coded suite name to be specified' do
+
+        allow_any_instance_of(Pathname).to receive(:exist?).and_return(false)
+        expect_any_instance_of(Beaker::CLI).to receive(:execute!).once
+        expect{subcommand.exec('tests')}.to_not raise_error
+      end
+
+      it 'errors when a resource is neither a valid file resource or suite name' do
+        allow_any_instance_of(Pathname).to receive(:exist?).and_return(false)
+        expect{subcommand.exec('blahblahblah')}.to raise_error(ArgumentError)
       end
     end
   end
