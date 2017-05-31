62856b708807c76584007b101cbda8fd2f393cca
Jenkins CI <ci@puppetlabs.com>
2017-05-31 13:46:00 -0700

    (BKR-994) Read subcommands options from home directory This patch changed options priority to read options from $HOME/.beaker/subcommands_options.yaml with the lowest priority after the presets. Added respective spec tests and modified others to fit this in the priority chain. Please note that as the priority hierarchy is changing there are some places in the module and their spec files that require modification in order to fit this in. The new options priority now (from highest to lowest): 1. ENV variables 2. ARGV variables 3. 'CONFIG' section of hosts file 4. Options file 5. Subcommand options 6. Config file from home directory (this patch) 7. Presets

--- lib/beaker/options/subcommand_options_file_parser.rb
+++ lib/beaker/options/subcommand_options_file_parser.rb
@@ -3,18 +3,30 @@ module Beaker
     #A set of functions to read options files
     module SubcommandOptionsParser
 
-      # @return [OptionsHash, Hash] returns an empty OptionHash or loads subcommand options yaml
-      #   from disk
-      def self.parse_subcommand_options(argv)
+      HOMEDIR_OPTIONS_FILE_PATH = ENV['HOME']+'/.beaker/subcommand_options.yaml'
+
+      def self.parse_options_file(options_file_path)
         result = OptionsHash.new
-        if Beaker::Subcommands::SubcommandUtil.execute_subcommand?(argv[0])
-          return result if argv[0] == 'init'
-          if Beaker::Subcommands::SubcommandUtil::SUBCOMMAND_OPTIONS.exist?
-           result = YAML.load_file(Beaker::Subcommands::SubcommandUtil::SUBCOMMAND_OPTIONS)
-          end
+        if File.exist?(options_file_path)
+          result = YAML.load_file(options_file_path)
         end
         result
       end
+
+      # @return [OptionsHash, Hash] returns an empty OptionHash or loads subcommand options yaml
+      #   from disk
+      def self.parse_subcommand_options(argv, home_dir=false)
+        result = OptionsHash.new
+        if Beaker::Subcommands::SubcommandUtil.execute_subcommand?(argv[0])
+          return result if argv[0] == 'init'
+          if home_dir
+            return SubcommandOptionsParser.parse_options_file(HOMEDIR_OPTIONS_FILE_PATH)
+          end
+          result = SubcommandOptionsParser.parse_options_file(Beaker::Subcommands::SubcommandUtil::SUBCOMMAND_OPTIONS)
+        end
+        result
+      end
+
     end
   end
 end
