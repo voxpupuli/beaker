724a7064dec6aa35c494575657a56b1e0cc2463e
Rishi Javia <rishi.javia.3895@gmail.com>
2017-06-29 08:16:57 -0700

    (BKR-1149) Use last ip from the list when hypervisor is vagrant (#1417)
    * Use last ip from the list when hypervisor is vagrant
    
    * Add spec tests

--- spec/beaker/host_prebuilt_steps_spec.rb
+++ spec/beaker/host_prebuilt_steps_spec.rb
@@ -516,10 +516,18 @@ describe Beaker do
     it "can exec the get_ip command" do
       host = make_host('name', { :stdout => "192.168.2.130\n" } )
 
-      expect( Beaker::Command ).to receive( :new ).with( "ip a|awk '/global/{print$2}' | cut -d/ -f1 | head -1", [], {:prepend_cmds=>nil, :cmdexe=>false} ).once
+      expect( Beaker::Command ).to receive( :new ).with( "ip a | awk '/global/{print$2}' | cut -d/ -f1 | head -1", [], {:prepend_cmds=>nil, :cmdexe=>false} ).once
 
       expect( subject.get_ip( host ) ).to be === "192.168.2.130"
 
+    end
+
+    it "can exec the get_ip command with tail with vagrant hypervisor" do
+      host = make_host('name', { :stdout => "192.168.2.131\n", :hypervisor => "vagrant" } )
+
+      expect( Beaker::Command ).to receive( :new ).with( "ip a | awk '/global/{print$2}' | cut -d/ -f1 | tail -1", [], {:prepend_cmds=>nil, :cmdexe=>false} ).once
+
+      expect( subject.get_ip( host ) ).to be === "192.168.2.131"
     end
 
   end
