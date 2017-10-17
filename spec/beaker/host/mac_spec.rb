9088548d603723afde52a1e5db0d0d87e45278d8
Michelle Schuknecht <michelle.schuknecht@puppet.com>
2017-10-17 11:50:08 -0700

    (maint) Updated mac spec test to use a supported mac platform

--- spec/beaker/host/mac_spec.rb
+++ spec/beaker/host/mac_spec.rb
@@ -7,7 +7,7 @@ module Mac
       if @platform
         { :platform => Beaker::Platform.new( @platform) }
       else
-        { :platform => Beaker::Platform.new( 'osx-10.9-x86_64' ) }
+        { :platform => Beaker::Platform.new( 'osx-10.12-x86_64' ) }
       end
     }
     let(:host)    { make_host( 'name', options.merge(platform) ) }
@@ -39,11 +39,11 @@ module Mac
       end
 
       it 'adds the version dot correctly if not supplied' do
-        @platform = 'osx-109-x86_64'
+        @platform = 'osx-10.12-x86_64'
         allow( host ).to receive( :link_exists? ) { true }
         release_path_end, release_file = host.puppet_agent_dev_package_info( 'PC3', 'pav3', :download_url => '' )
-        expect( release_path_end ).to match( /10\.9/ )
-        expect( release_file ).to match( /10\.9/ )
+        expect( release_path_end ).to match( /10\.12/ )
+        expect( release_file ).to match( /10\.12/ )
       end
 
       it 'runs the correct install for osx platforms (newest link format)' do
@@ -53,9 +53,9 @@ module Mac
         # verify the mac package name starts the name correctly
         expect( release_file ).to match( /^puppet-agent-pav4-/ )
         # verify the "newest hotness" is set correctly for the end of the mac package name
-        expect( release_file ).to match( /#{Regexp.escape("-1.osx10.9.dmg")}$/ )
+        expect( release_file ).to match( /#{Regexp.escape("-1.osx10.12.dmg")}$/ )
         # verify the release path end is set correctly
-        expect( release_path_end ).to be === "apple/10.9/PC4/x86_64"
+        expect( release_path_end ).to be === "apple/10.12/PC4/x86_64"
       end
 
       it 'runs the correct install for osx platforms (new link format)' do
@@ -65,9 +65,9 @@ module Mac
         # verify the mac package name starts the name correctly
         expect( release_file ).to match( /^puppet-agent-pav7-/ )
         # verify the "new hotness" is set correctly for the end of the mac package name
-        expect( release_file ).to match( /#{Regexp.escape("-1.mavericks.dmg")}$/ )
+        expect( release_file ).to match( /#{Regexp.escape("-1.sierra.dmg")}$/ )
         # verify the release path end isn't changed in the "new hotness" case
-        expect( release_path_end ).to be === "apple/10.9/PC7/x86_64"
+        expect( release_path_end ).to be === "apple/10.12/PC7/x86_64"
       end
 
       it 'runs the correct install for osx platforms (old link format)' do
@@ -77,7 +77,7 @@ module Mac
         # verify the mac package name starts the name correctly
         expect( release_file ).to match( /^puppet-agent-pav8-/ )
         # verify the old way is set correctly for the end of the mac package name
-        expect( release_file ).to match( /#{Regexp.escape("-osx-10.9-x86_64.dmg")}$/ )
+        expect( release_file ).to match( /#{Regexp.escape("-osx-10.12-x86_64.dmg")}$/ )
         # verify the release path end is set correctly to the older method
         expect( release_path_end ).to be === "apple/PC8"
       end
