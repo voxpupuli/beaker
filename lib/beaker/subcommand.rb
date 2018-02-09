3d9c1ac64f727830d8f4377d6ddc5e54dcc704e7
Melissa Stone <melissa@puppet.com>
2018-02-09 11:01:04 -0800

    (maint) Add --test-tag-and and --test-tag-or to beaker subcommand options

--- lib/beaker/subcommand.rb
+++ lib/beaker/subcommand.rb
@@ -54,6 +54,8 @@ module Beaker
     class_option :'debug-errors', :type => :boolean, :group => 'Beaker run'
     class_option :'exec_manual_tests', :type => :boolean, :group => 'Beaker run'
     class_option :'test-tag-exclude', :type => :string, :group => 'Beaker run'
+    class_option :'test-tag-and', :type => :string, :group => 'Beaker run'
+    class_option :'test-tag-or', type => :string, :group => 'Beaker run'
 
     # The following are listed as deprecated in beaker --help, but needed now for
     # feature parity for beaker 3.x.
