ade382b9ca103382ac5346d40c33065796e0923a
Tony Vu <tony.vu@puppet.com>
2018-02-12 13:23:00 -0800

    (maint) fix missing colon for subcommand option

--- lib/beaker/subcommand.rb
+++ lib/beaker/subcommand.rb
@@ -55,7 +55,7 @@ module Beaker
     class_option :'exec_manual_tests', :type => :boolean, :group => 'Beaker run'
     class_option :'test-tag-exclude', :type => :string, :group => 'Beaker run'
     class_option :'test-tag-and', :type => :string, :group => 'Beaker run'
-    class_option :'test-tag-or', type => :string, :group => 'Beaker run'
+    class_option :'test-tag-or', :type => :string, :group => 'Beaker run'
 
     # The following are listed as deprecated in beaker --help, but needed now for
     # feature parity for beaker 3.x.
