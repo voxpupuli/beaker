7775ad86c71a3b9bed944da47e77033cab3a56d5
Dakota Schneider <dakota.schneider@puppet.com>
2018-07-19 13:30:37 -0700

    (BKR-1492) Move pe_defaults to beaker-pe

--- lib/beaker/dsl/install_utils.rb
+++ lib/beaker/dsl/install_utils.rb
@@ -1,11 +1,9 @@
-require "beaker/dsl/install_utils/pe_defaults"
 require 'beaker-puppet'
 
 module Beaker
   module DSL
     # Collection of installation methods and support
     module InstallUtils
-      include DSL::InstallUtils::PEDefaults
       include BeakerPuppet::InstallUtils
     end
   end
