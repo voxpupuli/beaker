2d3d941faf9b7a8067c9fe5ed22f288c48966e96
Brandon High <brandon.high@puppet.com>
2019-11-05 11:26:54 -0800

    run reboot in parallel

--- smoke.rb
+++ smoke.rb
@@ -1,8 +1,12 @@
-@hosts.each do |host|
-  on host, 'echo hello'
-  puts "platform: #{host['platform']}"
+# @hosts.each do |host|
+#   on host, 'echo hello'
+#   puts "platform: #{host['platform']}"
+#   host.reboot
+#   on host, 'echo itworked'
+#   # puts "puppet user: #{puppet_user host}"
+#   # puts "puppet user: #{host.puppet['user']}"
+# end
+
+block_on @hosts, {:run_in_parallel => true} do |host|
   host.reboot
-  on host, 'echo itworked'
-  # puts "puppet user: #{puppet_user host}"
-  # puts "puppet user: #{host.puppet['user']}"
 end
