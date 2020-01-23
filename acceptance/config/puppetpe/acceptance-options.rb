1fda549bb6bfed102ff22944ad8933fe8a0e9683
Molly Waggett <molly.waggett@puppet.com>
2020-01-23 14:31:44 -0800

    (RE-13193) Migrate to Artifactory
    This commit replaces all references to enterprise.delivery.puppetlabs.net (aka
    saturn aka neptune) with Artifactory, as part of a larger migration.

--- acceptance/config/puppetpe/acceptance-options.rb
+++ acceptance/config/puppetpe/acceptance-options.rb
@@ -1,5 +1,5 @@
 {
     :pre_suite            => 'acceptance/pre_suite/pe/install.rb',
     :tests                => 'acceptance/tests/puppet',
-    :pe_dir               => 'http://neptune.puppetlabs.lan/archives/releases/3.7.2'
+    :pe_dir               => 'https://artifactory.delivery.puppetlabs.net/artifactory/generic_enterprise__local/archives/releases/2018.1.11'
 }.merge(eval File.read('acceptance/config/acceptance-options.rb'))
