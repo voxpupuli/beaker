voxpupuli/beaker

commit d7b6cebfa8b33835647cab7c5e51a3e21bbf7b07

added yard documentation to google compute code + new platform class
- we like docs!
- i should do this more often!
- please don't check for grammar

lib/beaker/host_prebuilt_steps.rb
@@ -71,8 +71,9 @@ module Beaker
     end
 
     #Validate that hosts are prepared to be used as SUTs, if packages are missing attempt to
-    #install them.  Verifies the presence of {HostPrebuiltSteps::PACKAGES} on all hosts and 
-    #{HostPrebuiltSteps::UNIX_PACKAGES} on unix platform hosts.
+    #install them.  Verifies the presence of {HostPrebuiltSteps::PACKAGES} on all hosts, 
+    #{HostPrebuiltSteps::UNIX_PACKAGES} on unix platform hosts and {HostPrebuiltSteps::SLES_PACKAGES}
+    #on sles (SUSE, Enterprise Linux) hosts.
     # @param [Host, Array<Host>, String, Symbol] host One or more hosts to act upon
     # @param [Hash{Symbol=>String}] opts Options to alter execution.
     # @option opts [Beaker::Logger] :logger A {Beaker::Logger} object
@@ -93,7 +94,6 @@ module Beaker
                 host.install_package pkg
               end
             end
-
           when host['platform'] !~ /(windows)|(aix)|(solaris)/
             UNIX_PACKAGES.each do |pkg|
               if not host.check_for_package pkg
@@ -343,6 +343,65 @@ module Beaker
       end
       hosts.each do |host|
         set_etc_hosts(host, etc_hosts)
+      end
+    end
+
+    #Update sshd_config on debian, ubuntu, centos, el, redhat and fedora boxes to allow for root login, does nothing on other platfoms
+    # @param [Host, Array<Host>, String, Symbol] host One or more hosts to act upon
+    # @param [Hash{Symbol=>String}] opts Options to alter execution.
+    # @option opts [Beaker::Logger] :logger A {Beaker::Logger} object
+    def enable_root_login host, opts
+      logger = opts[:logger]
+      if host.is_a? Array
+        host.map { |h| copy_ssh_to_root(h, opts) }
+      else
+        logger.debug "Update /etc/ssh/sshd_config to allow root login"
+        host.exec(Command.new("sudo su -c \"sed -i 's/PermitRootLogin no/PermitRootLogin yes/' /etc/ssh/sshd_config\""), {:pty => true}
+  )
+        #restart sshd
+        if host['platform'] =~ /debian|ubuntu/
+          host.exec(Command.new("sudo su -c \"service ssh restart\""), {:pty => true})
+        elsif host['platform'] =~ /centos|el-|redhat|fedora/
+          host.exec(Command.new("sudo su -c \"service sshd restart\""), {:pty => true})
+        else
+          @logger.warn("Attempting to update ssh on non-supported platform: #{host.name}: #{host['platform']}")
+        end
+      end
+    end
+
+    #Disable SELinux on centos, does nothing on other platforms
+    # @param [Host, Array<Host>, String, Symbol] host One or more hosts to act upon
+    # @param [Hash{Symbol=>String}] opts Options to alter execution.
+    # @option opts [Beaker::Logger] :logger A {Beaker::Logger} object
+    def disable_se_linux host, opts
+      logger = opts[:logger]
+      if host.is_a? Array
+        host.map { |h| copy_ssh_to_root(h, opts) }
+      else
+        if host['platform'] =~ /centos/
+          @logger.debug("Disabling se_linux on #{host.name}")
+          host.exec(Command.new("sudo su -c \"setenforce 0\""), {:pty => true})
+        else
+          @logger.warn("Attempting to disable SELinux on non-centos platform: #{host.name}: #{host['platform']}")
+        end
+      end
+    end
+
+    #Disable iptables on centos, does nothing on other platforms
+    # @param [Host, Array<Host>, String, Symbol] host One or more hosts to act upon
+    # @param [Hash{Symbol=>String}] opts Options to alter execution.
+    # @option opts [Beaker::Logger] :logger A {Beaker::Logger} object
+    def disable_iptables host, opts
+      logger = opts[:logger]
+      if host.is_a? Array
+        host.map { |h| copy_ssh_to_root(h, opts) }
+      else
+        if host['platform'] =~ /centos/
+          logger.debug("Disabling iptables on #{host.name}")
+          host.exec(Command.new("sudo su -c \"/etc/init.d/iptables stop\""), {:pty => true})
+        else
+          logger.warn("Attempting to disable iptables on non-centos platform: #{host.name}: #{host['platform']}")
+        end
       end
     end
 

lib/beaker/hypervisor.rb
@@ -12,7 +12,7 @@ module Beaker
     include HostPrebuiltSteps
 
     #Generates an array with all letters a thru z and numbers 0 thru 9
-    CHARMAP = [('a'..'z'),('0'..'9')].map{|r| r.to_a}.flatten
+    CHARMAP = ('a'..'z').to_a + ('0'..'9').to_a
 
     #Hypervisor creator method.  Creates the appropriate hypervisor class object based upon
     #the provided hypervisor type selected, then provisions hosts with hypervisor.
@@ -85,35 +85,16 @@ module Beaker
       if @options[:add_master_entry]
         add_master_entry(@hosts, @options)
       end
-   end
+    end
 
-   #Default validation steps to be run for a given hypervisor
-   def validate
-     validate_host(@hosts, @options)
-   end
+    #Default validation steps to be run for a given hypervisor
+    def validate
+      validate_host(@hosts, @options)
+    end
 
+    #Generate a random straing composted of letter and numbers
     def generate_host_name
       CHARMAP[rand(25)] + (0...14).map{CHARMAP[rand(CHARMAP.length)]}.join
-    end
-
-    def copy_ssh_to_root host
-      #make it possible to log in as root by copying the ssh dir to root's account
-      @logger.debug "Give root a copy of current host's keys"
-      if host['platform'] =~ /windows/
-        host.exec(Command.new('sudo su -c "cp -r .ssh /home/Administrator/."'))
-      else
-        host.exec(Command.new('sudo su -c "cp -r .ssh /root/."'), {:pty => true})
-      end
-    end
-
-    def hack_etc_hosts hosts
-      etc_hosts = "127.0.0.1\tlocalhost localhost.localdomain\n"
-      hosts.each do |host|
-        etc_hosts += "#{host['ip'].to_s}\t#{host[:vmhostname] || host.name}\n"
-      end
-      hosts.each do |host|
-        set_etc_hosts(host, etc_hosts)
-      end
     end
 
   end

lib/beaker/hypervisor/google_compute.rb
@@ -1,47 +1,39 @@
 require 'time'
 
 module Beaker
+  #Beaker support for the Google Compute Engine.
   class GoogleCompute < Beaker::Hypervisor
 
     SLEEPWAIT = 5
     #number of hours before an instance is considered a zombie
     ZOMBIE = 3
 
+    #Create a new instance of the Google Compute Engine hypervisor object
+    #@param [<Host>] google_hosts The array of google hosts to provision, may ONLY be of platforms /centos-6-.*/ and
+    #                             /debian-7-.*/.  We currently only support the Google Compute provided templates.
+    #@param [Hash{Symbol=>String}] options The options hash containing configuration values
+    #@option options [String] :gce_project The Google Compute Project name to connect to
+    #@option options [String] :gce_keyfile The location of the Google Compute service account keyfile
+    #@option options [String] :gce_password The password for the Google Compute service account key
+    #@option options [String] :gce_email The email address for the Google Compute service account
+    #@option options [String] :gce_machine_type A Google Compute machine type used to create instances, defaults to n1-highmem-2
+    #@option options [Integer] :timeout The amount of time to attempt execution before quiting and exiting with failure
     def initialize(google_hosts, options)
       @options = options
       @logger = options[:logger]
-      @google_hosts = google_hosts
+      @hosts = google_hosts
       @firewall = ''
       @gce_helper = GoogleComputeHelper.new(options)
     end
 
-    def allow_root_login host
-      @logger.debug "Update /etc/ssh/sshd_config to allow root login"
-      host.exec(Command.new("sudo su -c \"sed -i 's/PermitRootLogin no/PermitRootLogin yes/' /etc/ssh/sshd_config\""), {:pty => true})
-      #restart sshd
-      if host['platform'] =~ /debian|ubuntu/
-        host.exec(Command.new("sudo su -c \"service ssh restart\""), {:pty => true})
-      elsif host['platform'] =~ /centos|el-|redhat|fedora/
-        host.exec(Command.new("sudo su -c \"service sshd restart\""), {:pty => true})
-      else
-        raise "Attempting to update ssh on non-supported platform: #{host.name}: #{host['platform']}"
-      end
-    end
-
-    def disable_se_linux_and_firewall host
-      if host['platform'] =~ /centos/
-        @logger.debug("Disabling se_linux on #{host.name}")
-        host.exec(Command.new("sudo su -c \"setenforce 0\""), {:pty => true})
-        host.exec(Command.new("sudo su -c \"/etc/init.d/iptables stop\""), {:pty => true})
-      end
-    end
-
+    #Create and configure virtual machines in the Google Compute Engine, including their associated disks and firewall rules
+    #Currently ONLY supports Google Compute provided templates of CENTOS-6 and DEBIAN-7
     def provision
       try = 1
       attempts = @options[:timeout].to_i / SLEEPWAIT
       start = Time.now
 
-      #get machineType resource, used my all instances
+      #get machineType resource, used by all instances
       machineType = @gce_helper.get_machineType(start, attempts)
 
       #set firewall to open pe ports
@@ -49,20 +41,20 @@ module Beaker
       @firewall = generate_host_name
       @gce_helper.create_firewall(@firewall, network, start, attempts)
 
-      @logger.debug("Created firewall #{@firewall}")
+      @logger.debug("Created Google Compute firewall #{@firewall}")
 
 
-      @google_hosts.each do |host|
+      @hosts.each do |host|
         img = @gce_helper.get_latest_image(host[:platform], start, attempts)
         host['diskname'] = generate_host_name
         disk = @gce_helper.create_disk(host['diskname'], img, start, attempts)
-        @logger.debug("Created disk #{host.name}: #{host['diskname']}")
+        @logger.debug("Created Google Compute disk for #{host.name}: #{host['diskname']}")
 
         #create new host name
         host['vmhostname'] = generate_host_name
         #add a new instance of the image
         instance = @gce_helper.create_instance(host['vmhostname'], img, machineType, disk, start, attempts)
-        @logger.debug("Created instance #{host.name}: #{host['vmhostname']}")
+        @logger.debug("Created Google Compute instance for #{host.name}: #{host['vmhostname']}")
 
         #get ip for this host
         host['ip'] = instance['networkInterfaces'][0]['accessConfigs'][0]['natIP']
@@ -71,9 +63,10 @@ module Beaker
         default_user = host['user']
         host['user'] = 'google_compute'
 
-        disable_se_linux_and_firewall(host)
-        copy_ssh_to_root(host)
-        allow_root_login(host)
+        disable_se_linux(host, @options)
+        disable_iptables(host, @options)
+        copy_ssh_to_root(host, @options)
+        enable_root_login(host, @options)
         host['user'] = default_user
 
         #shut down connection, will reconnect on next exec
@@ -83,21 +76,24 @@ module Beaker
       end
     end
 
+    #Shutdown and destroy virtual machines in the Google Compute Engine, including their associated disks and firewall rules
     def cleanup()
       attempts = @options[:timeout].to_i / SLEEPWAIT
       start = Time.now
 
       @gce_helper.delete_firewall(@firewall, start, attempts) 
 
-      @google_hosts.each do |host|
+      @hosts.each do |host|
         @gce_helper.delete_instance(host['vmhostname'], start, attempts)
-        @logger.debug("Deleted instance #{host['vmhostname']} for #{host.name}")
+        @logger.debug("Deleted Google Compute instance #{host['vmhostname']} for #{host.name}")
         @gce_helper.delete_disk(host['diskname'], start, attempts)
-        @logger.debug("Deleted disk #{host['diskname']} for #{host.name}")
+        @logger.debug("Deleted Google Compute disk #{host['diskname']} for #{host.name}")
       end
 
     end
-
+ 
+    #Shutdown and destroy Google Compute instances (including their associated disks and firewall rules) 
+    #that have been alive longer than ZOMBIE hours.
     def kill_zombies(max_age = ZOMBIE)
       now = start = Time.now
       attempts = @options[:timeout].to_i / SLEEPWAIT

lib/beaker/platform.rb
@@ -1,8 +1,10 @@
 module Beaker
+  #This class create a Platform object inheriting from String.  It supports all String methods while adding 
+  #several platform-specific use cases.
   class Platform < String
-    #supported platforms
+    #Supported platforms
     PLATFORMS = /^(centos|fedora|debian|oracle|redhat|scientific|sles|ubuntu|windows|solaris|aix|el)\-.+\-.+$/
-
+    #Platform version numbers vs. codenames conversion hash
     PLATFORM_VERSION_CODES = 
       { :debian => { "wheezy"  => "7",
                      "squeeze" => "6",
@@ -15,6 +17,20 @@ module Beaker
                    },
       }
 
+    #Creates the Platform object.  Checks to ensure that the platform String provided meets the platform
+    #formatting rules.  Platforms name must be of the format /^OSFAMILY-VERSION-ARCH.*$/ where OSFAMILY is one of:
+    # * centos
+    # * fedora
+    # * debian
+    # * oracle
+    # * redhat
+    # * scientific
+    # * sles
+    # * ubuntu
+    # * windows
+    # * solaris
+    # * aix
+    # * el
     def initialize(name)
       if name !~ PLATFORMS
         raise ArgumentError, "Unsupported platform name #{name}"
@@ -22,6 +38,10 @@ module Beaker
       super
     end
 
+    # Returns the platform string with the platform version as a codename.  If no conversion is
+    # necessary then the original, unchanged platform String is returned.
+    # @example Platform.new('debian-7-xxx').with_version_codename == 'debian-wheezy-xxx'
+    # @return [String] the platform string with the platform version represented as a codename
     def with_version_codename
       name, version, extra = self.split('-', 3)
       PLATFORM_VERSION_CODES.each_key do |platform|
@@ -39,6 +59,10 @@ module Beaker
       [name, version, extra].join('-')
     end
 
+    # Returns the platform string with the platform version as a number.  If no conversion is necessary
+    # then the original, unchanged platform String is returned.
+    # @example Platform.new('debian-wheezy-xxx').with_version_number == 'debian-7-xxx'
+    # @return [String] the platform string with the platform version represented as a number
     def with_version_number
       name, version, extra = self.split('-', 3)
       PLATFORM_VERSION_CODES.each_key do |platform|

lib/beaker/shared.rb
@@ -1,4 +1,4 @@
-[ 'repetition', 'error_handler', 'host_handler', 'timed' ].each do |file|
+[ 'repetition', 'error_handler', 'host_role_parser', 'timed' ].each do |file|
   begin
     require "beaker/shared/#{file}"
   rescue LoadError

spec/beaker/hypervisor/vagrant_spec.rb
@@ -38,7 +38,7 @@ module Beaker
 
       generated_file = File.read( File.expand_path( File.join( path, "Vagrantfile") ) )
 
-      match = generated_file.match(/vb.customize \["modifyvm", :id, "--memory", "hello!"]/)
+      match = generated_file.match(/vb.customize \["modifyvm", :id, "--memory", "hello!"\]/)
 
       expect( match ).to_not be nil
 
@@ -50,7 +50,7 @@ module Beaker
         vagrant.should_receive( :set_etc_hosts ).with( host, "127.0.0.1\tlocalhost localhost.localdomain\nip.address.for.vm1\tvm1\nip.address.for.vm2\tvm2\nip.address.for.vm3\tvm3\n" ).once
       end
 
-      vagrant.hack_etc_hosts( @hosts )
+      vagrant.hack_etc_hosts( @hosts, options )
 
     end
 

commit df9a71a402ca4ed3ccbaea38e5b8f99b852c5421

Use correct logger object in disable_se_linux
Fixes: d7b6cebfa8b33835647cab7c5e51a3e21bbf7b07

lib/beaker/host_prebuilt_steps.rb
@@ -491,10 +491,10 @@ module Beaker
       logger = opts[:logger]
       block_on host do |host|
         if host['platform'] =~ /centos|el-|redhat|fedora|eos/
-          @logger.debug("Disabling se_linux on #{host.name}")
+          logger.debug("Disabling se_linux on #{host.name}")
           host.exec(Command.new("sudo su -c \"setenforce 0\""), {:pty => true})
         else
-          @logger.warn("Attempting to disable SELinux on non-supported platform: #{host.name}: #{host['platform']}")
+          logger.warn("Attempting to disable SELinux on non-supported platform: #{host.name}: #{host['platform']}")
         end
       end
     end