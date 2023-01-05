voxpupuli/beaker

commit dc0a2fb676d6caac6da3722106c4ee91434f169a

Fix Rubocop Performance/StringInclude

lib/beaker/host/pswindows/pkg.rb
@@ -27,7 +27,7 @@ module PSWindows::Pkg
   #Examine the host system to determine the architecture, overrides default host determine_if_x86_64 so that wmic is used
   #@return [Boolean] true if x86_64, false otherwise
   def determine_if_x86_64
-    (identify_windows_architecture =~ /64/) == 0
+    (identify_windows_architecture.include?('64')) == 0
   end
 
   private

lib/beaker/host/windows/pkg.rb
@@ -32,7 +32,7 @@ module Windows::Pkg
   #Examine the host system to determine the architecture, overrides default host determine_if_x86_64 so that wmic is used
   #@return [Boolean] true if x86_64, false otherwise
   def determine_if_x86_64
-    (identify_windows_architecture =~ /64/) == 0
+    (identify_windows_architecture.include?('64')) == 0
   end
 
   # Gets the path & file name for the puppet agent dev package on Windows

lib/beaker/host/unix/pkg.rb
@@ -477,7 +477,7 @@ module Unix::Pkg
       if arch == 'x86_64'
         arch = 'amd64'
       end
-      version = version[0,2] + '.' + version[2,2] if (variant =~ /ubuntu/ && !version.include?("."))
+      version = version[0,2] + '.' + version[2,2] if (variant.include?('ubuntu') && !version.include?("."))
       release_file = "/repos/apt/#{codename}/pool/#{puppet_collection}/p/puppet-agent/puppet-agent*#{arch}.deb"
       download_file = "puppet-agent-#{variant}-#{version}-#{arch}.tar.gz"
     when /^solaris$/

acceptance/tests/base/dsl/helpers/hocon_helpers_test.rb
@@ -35,7 +35,7 @@ test_name 'Hocon Helpers Test' do
         end
         fail('execution should not continue in failure mode')
       rescue ArgumentError => e
-        assert(e.to_s =~ /requires a filename/)
+        assert(e.to_s.include?('requires a filename'))
       else
         fail('No exception raised in failure mode')
       end
@@ -54,7 +54,7 @@ test_name 'Hocon Helpers Test' do
         hocon_file_edit_on(hosts, hocon_filename)
         fail('execution should not continue in failure mode')
       rescue ArgumentError => e
-        assert(e.to_s =~ /No block was provided/)
+        assert(e.to_s.include?('No block was provided'))
       else
         fail('No exception raised in failure mode')
       end

acceptance/tests/base/host/group_test.rb
@@ -1,6 +1,6 @@
 test_name 'Group Test' do
   step "#group_get: has an Administrators group on Windows" do
-    hosts.select { |h| h['platform'] =~ /windows/ }.each do |host|
+    hosts.select { |h| h['platform'].include?('windows') }.each do |host|
       host.group_get('Administrators') do |result|
         refute_match(result.stdout, '1376', 'Output indicates Administrators not found')
       end
@@ -8,7 +8,7 @@ test_name 'Group Test' do
   end
 
   step "#group_get: should not have CroMags group on Windows" do
-    hosts.select { |h| h['platform'] =~ /windows/ }.each do |host|
+    hosts.select { |h| h['platform'].include?('windows') }.each do |host|
       assert_raises Beaker::Host::CommandFailure do
         host.group_get('CroMags') { |result| }
       end

acceptance/tests/base/host/packages.rb
@@ -11,9 +11,9 @@ def get_host_pkg(host)
       Beaker::HostPrebuiltSteps::DEBIAN_PACKAGES
     when /cumulus/.match?(host['platform'])
       Beaker::HostPrebuiltSteps::CUMULUS_PACKAGES
-    when (host['platform'] =~ /windows/ and host.is_cygwin?)
+    when (host['platform'].include?('windows') and host.is_cygwin?)
       Beaker::HostPrebuiltSteps::WINDOWS_PACKAGES
-    when (host['platform'] =~ /windows/ and not host.is_cygwin?)
+    when (host['platform'].include?('windows') and not host.is_cygwin?)
       Beaker::HostPrebuiltSteps::PSWINDOWS_PACKAGES
     when /freebsd/.match?(host['platform'])
       Beaker::HostPrebuiltSteps::FREEBSD_PACKAGES
@@ -66,7 +66,7 @@ hosts.each do |host|
   cmdline_args = ''
   # Newer vmpooler hosts created by Packer templates, and running Cygwin 2.4,
   # must have these switches passed
-  cmdline_args = '--local-install --download' if (host['platform'] =~ /windows/ and host.is_cygwin?)
+  cmdline_args = '--local-install --download' if (host['platform'].include?('windows') and host.is_cygwin?)
   host.install_package(package, cmdline_args)
   assert(host.check_for_package(package), "'#{package}' should be installed")
 

acceptance/tests/base/host/user_test.rb
@@ -1,6 +1,6 @@
 test_name 'User Test' do
   step "#user_get: has an Administrator user on Windows" do
-    hosts.select { |h| h['platform'] =~ /windows/ }.each do |host|
+    hosts.select { |h| h['platform'].include?('windows') }.each do |host|
       host.user_get('Administrator') do |result|
         refute_match(result.stdout, 'NET HELPMSG', 'Output indicates Administrator not found')
       end
@@ -8,7 +8,7 @@ test_name 'User Test' do
   end
 
   step "#user_get: should not have CaptainCaveman user on Windows" do
-    hosts.select { |h| h['platform'] =~ /windows/ }.each do |host|
+    hosts.select { |h| h['platform'].include?('windows') }.each do |host|
       assert_raises Beaker::Host::CommandFailure do
         host.user_get('CaptainCaveman') { |result| }
       end

lib/beaker/dsl/helpers/host_helpers.rb
@@ -193,7 +193,7 @@ module Beaker
         # @return [Result] Returns the result of the SCP operation
         def scp_to host, from_path, to_path, opts = {}
           block_on host do | host |
-            if host['platform'] =~ /windows/ && to_path.match('`cygpath')
+            if host['platform'].include?('windows') && to_path.match('`cygpath')
               result = on host, "echo #{to_path}"
               to_path = result.raw_output.chomp
             end
@@ -215,7 +215,7 @@ module Beaker
         # @return [Result] Returns the result of the rsync operation
         def rsync_to host, from_path, to_path, opts = {}
           block_on host do | host |
-            if host['platform'] =~ /windows/ && to_path.match('`cygpath')
+            if host['platform'].include?('windows') && to_path.match('`cygpath')
               result = host.echo "#{to_path}"
               to_path = result.raw_output.chomp
             end

commit d35a34b3a6eb46a78f4f37a1833aad8723da6c13

Correct Windows 64 check
While refactoring this bug crept in.

Fixes: dc0a2fb676d6caac6da3722106c4ee91434f169a

lib/beaker/host/pswindows/pkg.rb
@@ -27,7 +27,7 @@ module PSWindows::Pkg
   #Examine the host system to determine the architecture, overrides default host determine_if_x86_64 so that wmic is used
   #@return [Boolean] true if x86_64, false otherwise
   def determine_if_x86_64
-    (identify_windows_architecture.include?('64')) == 0
+    identify_windows_architecture.include?('64')
   end
 
   private

lib/beaker/host/windows/pkg.rb
@@ -32,7 +32,7 @@ module Windows::Pkg
   #Examine the host system to determine the architecture, overrides default host determine_if_x86_64 so that wmic is used
   #@return [Boolean] true if x86_64, false otherwise
   def determine_if_x86_64
-    (identify_windows_architecture.include?('64')) == 0
+    identify_windows_architecture.include?('64')
   end
 
   # Gets the path & file name for the puppet agent dev package on Windows