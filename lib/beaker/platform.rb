5ff4d11a7bf51d59a86e5230bb633bc155c4d71c
Shubham Shinde <shubham.shinde@perforce.com>
2025-03-11 16:05:51 +0530

    (PA-7210) Add support for amazonfips

--- lib/beaker/platform.rb
+++ lib/beaker/platform.rb
@@ -3,7 +3,7 @@ module Beaker
   # all String methods while adding several platform-specific use cases.
   class Platform < String
     # Supported platforms
-    PLATFORMS = /^(alpine|amazon|(free|open)bsd|osx|centos|fedora|debian|oracle|redhat|redhatfips|scientific|opensuse|sles|ubuntu|windows|solaris|aix|archlinux|el)\-.+\-.+$/
+    PLATFORMS = /^(alpine|amazon(fips)?|(free|open)bsd|osx|centos|fedora|debian|oracle|redhat|redhatfips|scientific|opensuse|sles|ubuntu|windows|solaris|aix|archlinux|el)\-.+\-.+$/
     # Platform version numbers vs. codenames conversion hash
     PLATFORM_VERSION_CODES =
       { :debian => { "forky" => "14",
@@ -38,6 +38,7 @@ module Beaker
     # provided meets the platform formatting rules.  Platforms name must be of
     # the format /^OSFAMILY-VERSION-ARCH.*$/ where OSFAMILY is one of:
     # * amazon
+    # * amazonfips
     # * freebsd
     # * openbsd
     # * osx
@@ -127,7 +128,7 @@ module Beaker
         @version.to_i >= 11 ? %w[curl] : %w[CSWcurl wget]
       when 'archlinux'
         %w[curl net-tools openssh]
-      when 'amazon', 'fedora'
+      when 'amazon', 'amazonfips', 'fedora'
         ['iputils']
       when 'aix', 'osx', 'windows'
         []
