aeb21f92228c51b27e23660b311cd33a7c9ecf01
Tim Meusel <tim@bastelfreak.de>
2025-05-15 22:00:08 +0200

    Drop unused code coverage/generate ruby matrix

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,27 +1,3 @@
-begin
-  require 'simplecov'
-  require 'simplecov-console'
-  require 'codecov'
-rescue LoadError
-else
-  SimpleCov.start do
-    track_files 'lib/**/*.rb'
-
-    add_filter '/spec'
-
-    enable_coverage :branch
-
-    # do not track vendored files
-    add_filter '/vendor'
-    add_filter '/.vendor'
-  end
-
-  SimpleCov.formatters = [
-    SimpleCov::Formatter::Console,
-    SimpleCov::Formatter::Codecov,
-  ]
-end
-
 require 'beaker'
 require 'fakefs/spec_helpers'
 require 'mocks'
