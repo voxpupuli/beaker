d1aa72a9e2e309aebb82402e73abf877ed2a0817
Ewoud Kohl van Wijngaarden <ewoud@kohlvanwijngaarden.nl>
2025-08-12 16:33:17 +0200

    Drop is_x86_64? and determine_if_x86_64
    These helpers are only used by beaker-puppet and not generally useful.
    They completely ignore ARM and other architectures. The distinction
    between i386 and x86_64 is also largely irrelevant now that i386 is
    effectively dead.

--- lib/beaker/host/pswindows/pkg.rb
+++ lib/beaker/host/pswindows/pkg.rb
@@ -24,12 +24,6 @@ module PSWindows::Pkg
     0
   end
 
-  # Examine the host system to determine the architecture, overrides default host determine_if_x86_64 so that wmic is used
-  # @return [Boolean] true if x86_64, false otherwise
-  def determine_if_x86_64
-    identify_windows_architecture.include?('64')
-  end
-
   private
 
   # @api private
